116e17f48b8211a1482170d1b274e3c06106675d
Brian Garton <gartonb@gmail.com>
2022-09-17 13:19:20 +0700

    Set up before start

---
+++ tetris-game/src/components/Tetris.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Stage from "./Stage";
+import Display from "./Display";
+import StartButton from "./StartButton";
+
+const Tetris = () => {
+  return (
+    <div>Tetris</div>
+  )
+}
+
+export default Tetris
