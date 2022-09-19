f831e04789a64c64f318ccd2cfc0d3fcdc320a00
Brian Garton <gartonb@gmail.com>
2022-09-19 12:19:12 +0700

    added Board.js and useBoard.js

---
+++ tetris-game/src/hooks/useBoard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useState } from "react";
+
+export const useBoard = ({ rows, columns }) => {
+  const [board, setBoard] = useState([]);
+  
+    return (
+    <div>useBoard</div>
+  )
+}
