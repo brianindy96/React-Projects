ee76edecabcaf06f14ce28e9c77315ee0cc21dbb
Brian Garton <gartonb@gmail.com>
2022-09-19 12:36:17 +0700

    save

--- tetris-game/src/hooks/useGameOver.js
+++ tetris-game/src/hooks/useGameOver.js
@@ -4,6 +4,7 @@ import { useState, useCallBack } from 'react'
 export const useGameOver = () => {
   const [gameOver] = useState(true);
   const [setGameOver] = useState(true);
+  
   // useCallBack, because we want it to get executed only when we want it.
   const resetGameOver = useCallBack(() => {
     setGameOver(false);
