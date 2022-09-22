469bf8e74886c51ca7dc0392d5fd08dbb280f52e
Brian Garton <gartonb@gmail.com>
2022-09-22 22:36:32 +0700

    Form created

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import Overview from './components/Overview';
+import { useState } from 'react'
 
 function App() {
+
+  const [task, setTask] = useState({
+    task: {
+      text: '',
+    },
+    tasks: [], 
+  });
+
   return (
     <div className="App">
-      
+        <h1>Tasks</h1>
+        <br />
+        <form action="">
+          <label htmlFor="">Write a Task:</label>
+          <br />
+          <input type="text" id='taskInput' />
+          <button type="submit">Add Task</button>
+        </form>
     </div>
   );
 }
---
+++ task-app/src/components/Overview.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Overview.css'
+
+const Overview = () => {
+  return (
+    <div className="Overview">
+        <h1>Tasks</h1>
+    </div>
+  )
+}
+
+export default Overview
