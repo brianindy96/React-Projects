e037e7c4cea1cdb0473c41bf81112759ab7e34a8
Brian Garton <gartonb@gmail.com>
2022-09-23 18:37:20 +0700

    set handleSubmit and handleChange function

--- task-app/src/App.js
+++ task-app/src/App.js
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
-// import { useState } from 'react'
 
 const App = () => {
   // // state of arrays that takes in the whole tasks 
-  // const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState([]);
   // // state that takes in the task from input field
-  // const [task, setTask] = useState('');
+  const [task, setTask] = useState('');
+
+  
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    //If task does not equal an empty string, then setTasks to the input value.
+    if(task !== ''){
+      // we will update the 'tasks' state by putting in the input value from what we already have
+      setTasks([
+        // whatever we have in the current state
+        ...tasks,
+        {
+          id: tasks.length+1,
+          text: task.trim(),
+        }
+      ])
+    }
+
+    //clear Input
+
+    setTask('');
+
+  }
+  
+  const handleChange = (e) => {
+    setTask(e.target.value);
+
+    // console.log({task})
+  }
 
   return (
     <div className='App'>
@@ -16,8 +44,11 @@ const App = () => {
           <input 
           type="text" 
           placeholder='write here...'
+          onChange={handleChange}
           />
-          <button type="submit">
+          <button 
+          type="submit"
+          onSubmit={handleSubmit}>
             Add Task
           </button>
         </form>
