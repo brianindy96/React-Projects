b5490181651a3d1d2d8de518588243a86f9fd357
Brian Garton <gartonb@gmail.com>
2022-09-29 10:33:52 +0700

    unedited form created

--- task-app-template/src/App.js
+++ task-app-template/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+
+  // define the states of tasks and task's input value
+ const [tasks, setTasks] = useState([]);
+ const [task, setTask] = useState('');
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <form className="task-form" action="">
+      <label htmlFor="">Write down your Task:</label>
+      <input 
+      type="text" 
+      name="" 
+      placeholder='write down your task'
+       />
+      <button type="submit">Add Task</button>
+    </form>
+      
+    {/* UI of Task Lists */}
+    <ul>
+      {}
+    </ul>
     </div>
   );
 }
