ff71fc62301b10870c0fcf0bac21cd58fd7d57bc
Brian Garton <gartonb@gmail.com>
2022-09-29 16:08:56 +0700

    name for input added

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -1,71 +1,107 @@
 import './App.css';
+import { useState } from 'react'
 
 import React from 'react'
 
 const App = () => {
+
+  const [person, setPerson] = useState([]);
+
+  const [state, setState] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNum: '',
+  });
+
+  const handleInputChange = (e) =>{
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+
+    console.log(state);
+  }
+
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+  }
+  
   return (
     <div className='App'>
-        <div className='General'>
-          <form action="" className="general-info" name='general-info'>
+        <div className='Form'>
+          <form onSubmit={handleSubmit} action="" className="general-info" name='general-info'>
             {/* General Information */}
+            <div className='General'>
               <h2>General Information</h2>
-              {/* First Name */}
-              <label htmlFor="firstNameInput">First Name:</label>
-              <input 
-              id="firstNameInput"
-              type="text"
-              required
-              placeholder='write your first name here..' />
-              {/* Last Name */}
-              <label htmlFor="lastNameInput">Last Name:</label>
-              <input
-              id="lastNameInput" 
-              required
-              type="text"
-              placeholder='write your first name here..' />
-              {/* Email */}
-              <label htmlFor="emailInput">Email: </label>
-              <input 
-              id="emailInput"
-              required
-              type="email"
-              placeholder='write your email here..' />
-              {/* Phone Number */}
-              <label htmlFor="phoneNumInput">Phone Number:</label>
-              <input 
-              required
-              id="phoneNumInput"
-              type="text"
-              placeholder='please input your country code e.g. +xx' />
+                {/* First Name */}
+                <label htmlFor="firstNameInput">First Name:</label>
+                <input 
+                onChange={handleInputChange}
+                name="firstName"
+                id="firstNameInput"
+                type="text"
+                required
+                placeholder='write your first name here..' />
+                {/* Last Name */}
+                <label htmlFor="lastNameInput" >Last Name:</label>
+                <input
+                name="lastName"
+                onChange={handleInputChange}
+                id="lastNameInput" 
+                required
+                type="text"
+                placeholder='write your first name here..' />
+                {/* Email */}
+                <label htmlFor="emailInput">Email: </label>
+                <input 
+                name="email"
+                onChange={handleInputChange}
+                id="emailInput"
+                required
+                type="email"
+                placeholder='write your email here..' />
+                {/* Phone Number */}
+                <label htmlFor="phoneNumInput">Phone Number:</label>
+                <input 
+                name="phoneNum"
+                onChange={handleInputChange}
+                required
+                id="phoneNumInput"
+                type="text"
+                placeholder='please input your country code e.g. +xx' />
+            </div>
+            <div className='Education'>
+              {/* Education */}
+                <h2>Education</h2>
+                {/* School name */}
+                <label htmlFor="schoolName">School:</label>
+                <input 
+                id="schoolName"
+                type="text"
+                placeholder='write your school name here..' />
+                {/* Degree */}
+                <label htmlFor="degreeName">Degree:</label>
+                <input
+                id="degreeName" 
+                type="text"
+                placeholder='write your highest degree here..' />
+                {/* First enrolled date*/}
+                <label htmlFor="date-enrolled">Date first enrolled : </label>
+                <input 
+                id="date-enrolled"
+                type="date"
+                />
+                {/* Graduation Date */}
+                <label htmlFor="date-graduation">Graduation date : </label>
+                <input 
+                id="date-graduation"
+                type="date"
+                />
+            </div>
             
-            {/* Education */}
-              <h2>Education</h2>
-              {/* School name */}
-              <label htmlFor="schoolName">School:</label>
-              <input 
-              id="schoolName"
-              type="text"
-              placeholder='write your school name here..' />
-              {/* Degree */}
-              <label htmlFor="degreeName">Degree:</label>
-              <input
-              id="degreeName" 
-              type="text"
-              placeholder='write your highest degree here..' />
-              {/* First enrolled date*/}
-              <label htmlFor="date-enrolled">Date first enrolled : </label>
-              <input 
-              id="date-enrolled"
-              type="date"
-              />
-              {/* Graduation Date */}
-              <label htmlFor="date-graduation">Graduation date : </label>
-              <input 
-              id="date-graduation"
-              type="date"
-              />
-
-            {/* Work Experience */}
+            <div className="Work">
+              {/* Work Experience */}
               <h2>Work Experiences</h2>
               {/* Company Name */}
               <label htmlFor="companyName">Company Name:</label>
@@ -91,6 +127,8 @@ const App = () => {
               id="date-endwork"
               type="date"
               />
+            </div>
+            
             <br />
           <button type="submit">Submit</button>
           </form>
