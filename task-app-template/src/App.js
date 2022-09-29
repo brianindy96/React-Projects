e587bfa0eb32929b5d522b5953dba46b4881878e
Brian Garton <gartonb@gmail.com>
2022-09-29 10:57:46 +0700

    set handleChange and handleSubmitBtn done

--- task-app-template/src/App.js
+++ task-app-template/src/App.js
@@ -11,32 +11,51 @@ const App = () => {
  const handleSubmitBtn = (e) =>{
   e.preventDefault();
 
+  // if task is not empty, return a new state 
+  if (task !== ''){
+    setTasks([...tasks, {
+      id: tasks.length + 1,
+      // takes in the text from task state, and trims the spaces in between
+      text: task.trim(),
+    }])
+  }
 
+  setTask('');
  }
 
  //function that handles input changes
+ // then sets this function to input form with OnChange eventlistener
+ const handleInputChange = (e) =>{
+    // takes in the input value into the task state
+    setTask(e.target.value)
+ }
 
 
   return (
     <div className="App">
 
-    <form className="task-form" action="" onSubmit={handleSubmitBtn}>
-      <label htmlFor="">Write down your Task:</label>
-      <input 
-      type="text" 
-      name="" 
-      placeholder='write down your task'
-       />
-      <button type="submit">Add Task</button>
-    </form>
-      
-    {/* UI of Task Lists */}
-    <ul>
-      {/* {tasks.map((task) =>{
-        <li>{task.id}</li>
-        <li>{task.text}</li>
-      })} */}
-    </ul>
+      <form className="task-form" action="" onSubmit={handleSubmitBtn}>
+        <label htmlFor="">Write down your Task:</label>
+        <input 
+        type="text" 
+        name="" 
+        placeholder='write down your task'
+        onChange={handleInputChange}
+        value={task}
+        />
+        <button type="submit">Add Task</button>
+      </form>
+        
+      {/* UI of Task Lists */}
+      <div className="tasks-ui">
+        <ol>
+            {tasks.map((task) =>(
+              // when mapping arrays, always refer KEY TO ELEMENT
+              <li key={task.id}>{task.text}</li>
+            ))}
+          </ol>
+      </div>
+          
     </div>
   );
 }
