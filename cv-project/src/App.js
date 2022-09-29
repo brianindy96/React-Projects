515acb17d9065ee0c58fcff79144f6d7be831e9e
Brian Garton <gartonb@gmail.com>
2022-09-29 15:33:49 +0700

    Work Experiences

--- cv-project/src/App.js
+++ cv-project/src/App.js
@@ -7,62 +7,92 @@ const App = () => {
     <div className='App'>
         <div className='General'>
           <form action="" className="general-info" name='general-info'>
-            <h2>General Information</h2>
-            {/* First Name */}
-            <label htmlFor="firstNameInput">First Name:</label>
-            <input 
-            id="firstNameInput"
-            type="text"
-            required
-            placeholder='write your first name here..' />
-            {/* Last Name */}
-            <label htmlFor="lastNameInput">Last Name:</label>
-            <input
-            id="lastNameInput" 
-            required
-            type="text"
-            placeholder='write your first name here..' />
-            {/* Email */}
-            <label htmlFor="emailInput">Email: </label>
-            <input 
-            id="emailInput"
-            required
-            type="email"
-            placeholder='write your email here..' />
-            {/* Phone Number */}
-            <label htmlFor="phoneNumInput">Phone Number:</label>
-            <input 
-            required
-            id="phoneNumInput"
-            type="text"
-            placeholder='please input your country code e.g. +xx' />
-
-            <h2>Education</h2>
-            {/* School name */}
-            <label htmlFor="schoolName">School:</label>
-            <input 
-            id="schoolName"
-            type="text"
-            placeholder='write your school name here..' />
-            {/* Degree */}
-            <label htmlFor="degreeName">Degree:</label>
-            <input
-            id="degreeName" 
-            type="text"
-            placeholder='write your highest degree here..' />
-            {/* First enrolled date*/}
-            <label htmlFor="date-enrolled">Date first enrolled : </label>
-            <input 
-            id="date-enrolled"
-            type="date"
-            placeholder='write your email here..' />
-            {/* Graduation Date */}
-            <label htmlFor="date-graduation">Graduation date : </label>
-            <input 
-            id="date-graduation"
-            type="date"
-            placeholder='write your email here..' />
+            {/* General Information */}
+              <h2>General Information</h2>
+              {/* First Name */}
+              <label htmlFor="firstNameInput">First Name:</label>
+              <input 
+              id="firstNameInput"
+              type="text"
+              required
+              placeholder='write your first name here..' />
+              {/* Last Name */}
+              <label htmlFor="lastNameInput">Last Name:</label>
+              <input
+              id="lastNameInput" 
+              required
+              type="text"
+              placeholder='write your first name here..' />
+              {/* Email */}
+              <label htmlFor="emailInput">Email: </label>
+              <input 
+              id="emailInput"
+              required
+              type="email"
+              placeholder='write your email here..' />
+              {/* Phone Number */}
+              <label htmlFor="phoneNumInput">Phone Number:</label>
+              <input 
+              required
+              id="phoneNumInput"
+              type="text"
+              placeholder='please input your country code e.g. +xx' />
             
+            {/* Education */}
+              <h2>Education</h2>
+              {/* School name */}
+              <label htmlFor="schoolName">School:</label>
+              <input 
+              id="schoolName"
+              type="text"
+              placeholder='write your school name here..' />
+              {/* Degree */}
+              <label htmlFor="degreeName">Degree:</label>
+              <input
+              id="degreeName" 
+              type="text"
+              placeholder='write your highest degree here..' />
+              {/* First enrolled date*/}
+              <label htmlFor="date-enrolled">Date first enrolled : </label>
+              <input 
+              id="date-enrolled"
+              type="date"
+              />
+              {/* Graduation Date */}
+              <label htmlFor="date-graduation">Graduation date : </label>
+              <input 
+              id="date-graduation"
+              type="date"
+              />
+
+            {/* Work Experience */}
+              <h2>Work Experiences</h2>
+              {/* Company Name */}
+              <label htmlFor="companyName">Company Name:</label>
+              <input 
+              id="companyName"
+              type="text"
+              placeholder='write your Company Name here..' />
+              {/* Position */}
+              <label htmlFor="positionName">Position Title:</label>
+              <input
+              id="positionName" 
+              type="text"
+              placeholder='write your position title here..' />
+              {/* First enrolled date*/}
+              <label htmlFor="date-jobbegin">Date first enrolled : </label>
+              <input 
+              id="date-jobbegin"
+              type="date"
+              />
+              {/* Graduation Date */}
+              <label htmlFor="date-endwork">End date : </label>
+              <input 
+              id="date-endwork"
+              type="date"
+              />
+            <br />
+          <button type="submit">Submit</button>
           </form>
         </div>
     </div>
