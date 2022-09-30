967b5d23e3b3d18473295a03e95508b968caf304
Brian Garton <gartonb@gmail.com>
2022-09-30 19:57:30 +0700

    Card.css

--- memory-game-template/src/components/Card.js
+++ memory-game-template/src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./Card.css";
 
 const Card = ({ card }) => {
   return (
-    <div className="card" key={card.id}>
+    <div className="card">
         <div>
             <img className="front" src={card.src} alt="Front of Card Img" />
             <img className="back" src="img/nasa-Yj1M5riCKk4-unsplash.jpg" alt="card back" />
