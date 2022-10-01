4d01694ee42d810a79105a5d4003ab5509e5698c
Brian Garton <gartonb@gmail.com>
2022-10-01 13:53:32 +0700

    deleted CRUD Application and tetris

--- tetris-game/src/business/Cell.js
+++
@@ -1,7 +0,0 @@
-// When there is cell is just passing down it will not be occupied
-// Once there is a collision it will be true
-
-export const defaultCell = {
-    occupied: false,
-    className: ""
-}
--- tetris-game/src/hooks/useBoard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useState } from "react";
-import { buildBoard } from "/src/business/Board"
-
-export const useBoard = ({ rows, columns }) => {
-  const [board] = useState(buildBoard({ rows, columns }));
-  
-    return [board]
-}
