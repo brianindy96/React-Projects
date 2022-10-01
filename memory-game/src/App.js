394f47457cc3de29eb85c644e39fc5a65e5cbad2
Brian Garton <gartonb@gmail.com>
2022-10-01 14:40:27 +0700

    basic shuffleCards function

--- memory-game/src/App.js
+++ memory-game/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 // Step-by-Step
+
 
 // 1. Shuffling Cards function
 // 2. Creating a card grid
@@ -14,11 +16,45 @@ import './App.css';
 // 10. automatically start game on page is loaded
 // 11. set choices value to null every time start new game
 
+
+const cardImgs = [
+  {"src" : "/img/berlin.jpg"},
+  {"src" : "/img/london.jpg"},
+  {"src" : "/img/osaka.jpg"},
+  {"src" : "/img/newyork.jpg"},
+  {"src" : "/img/paris.jpg"},
+  {"src" : "/img/rome.jpg"},
+]
+
 function App() {
+
+  // States
+
+  const [cards, setCards] = useState([]);
+
+  // 1. Shuffling Cards function
+    // 1.1 Bring all the card Objects into array
+    // 1.2 Scramble it by sorting with Math.random()
+    // 1.3 assigns a new id to each object
+
+  const shuffleCards = () =>{
+    // 1.1 Bring all the card Objects into array
+    const shuffledCards = [...cardImgs, ...cardImgs]
+    // 1.2 Scramble it by sorting with Math.random()
+    .sort(() => Math.random()-0.5)
+    // 1.3 assigns a new id to each object
+    .map((card)=> ({...card, id: Math.random().toFixed(3)}))
+
+    console.log(shuffledCards);
+  }
+
   return (
     <div className="App">
       <h1>Memory Game</h1>
-      <button className='newGameBtn'>New Game</button>
+      <button 
+      className='newGameBtn'
+      onClick={shuffleCards}
+      >New Game</button>
 
     </div>
   );
