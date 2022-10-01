aa660e8478bc6877df5124cbd540981676a1d024
Brian Garton <gartonb@gmail.com>
2022-10-01 14:04:16 +0700

    Pseudocode

---
+++ memory-game/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+// Step-by-Step
+
+// 1. Shuffling Cards function
+// 2. Creating a card grid
+// 3. Creating a card component
+// 4. Making Card Choices
+// 5. Comparing Card Choices
+// 6. Matching Cards
+// 7. Flipping Cards
+// 8. Flipping Animation
+// 9. Disabling Card Selections
+// 10. automatically start game on page is loaded
+// 11. set choices value to null every time start new game
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
