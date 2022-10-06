aa3888491718feeb0e6a58ddbae206a88561ad43
Brian Garton <gartonb@gmail.com>
2022-10-06 11:42:12 +0200

    ImageContainer and Image with src added and styled

--- e-commerce-template/src/components/Slider.jsx
+++ e-commerce-template/src/components/Slider.jsx
@@ -9,6 +9,8 @@ const Container = styled.div`
     display: flex;
 `
 
+// Arrows
+
 const Arrow = styled.div`
     height: 50px;
     width: 50px;
@@ -25,6 +27,40 @@ const Arrow = styled.div`
     /* sending props */
     /* left: ${props => props.direction === "back" && "10px"}; */
     right: ${props => props.direction === "forward" && "10px"};
+    cursor: pointer;
+    opacity: 0.5;
+`;
+
+// Slides 
+
+
+const Wrapper = styled.div`
+    height: 100%;
+    background-color: coral;
+`;
+
+const Slide = styled.div`
+    display: flex;
+    width: 100vw;
+    height: 85vh;
+    align-items: center;
+`
+
+const ImgContainer = styled.div`
+    flex: 1;
+    border: 1px solid white;
+    /* Height 100% to its parent */
+    height: 100%;
+`
+
+const Image = styled.img`
+    height: 80%;
+`;
+
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 50px;
+    border: 1px solid purple;
 `
 
 const Slider = () => {
@@ -33,6 +69,14 @@ const Slider = () => {
         <Arrow direction="back">
             <ArrowBackIosIcon style={{color: "#fff"}}/>
         </Arrow>
+        <Wrapper>
+            <Slide>
+                <ImgContainer>
+                    <Image src="/img/shoppingone.png" />
+                </ImgContainer>
+                <InfoContainer />
+            </Slide>
+        </Wrapper>
         <Arrow direction="forward">
             <ArrowForwardIosIcon style={{color: "#fff"}}/>
         </Arrow>
