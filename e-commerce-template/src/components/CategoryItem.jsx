5f66c49ea901176df9448f1f1d7d4592b6ee5c40
Brian Garton <gartonb@gmail.com>
2022-10-06 20:24:11 +0200

    styled CategoryItem.jsx

--- e-commerce-template/src/components/CategoryItem.jsx
+++ e-commerce-template/src/components/CategoryItem.jsx
@@ -2,13 +2,55 @@ import React from 'react'
 import styled from 'styled-components';
 
 const Container = styled.div`
-    
-`
+    flex: 1;
+    /* text-align: center; */
+    margin: 3px;
+    position: relative;
+`;
+
+const Image = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
+
+const Info = styled.div`
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    /* background-color: yellow; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+`;
+
+const Title = styled.h1`
+    color: white;
+    font-weight: bold;
+    margin-bottom: 20px;
+
+`;
+
+const Button = styled.button`
+    padding: 10px;
+    border: none;
+    background-color: white;
+    color: gray;
+    cursor: pointer;
+    font-weight: bold;
+`;
 
 const CategoryItem = ({ item }) => {
   return (
     <Container>
-        Hello
+        <Image src={item.img} />
+        <Info>
+            <Title>{item.title}</Title>
+            <Button>SHOP NOW</Button>
+        </Info>
     </Container>
   )
 }
