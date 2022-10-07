55913ee5b6fd43799b2ad2149a1a7de324569d4a
Brian Garton <gartonb@gmail.com>
2022-10-07 10:56:37 +0200

    Newsletter styled

--- e-commerce-template/src/components/Newsletter.jsx
+++ e-commerce-template/src/components/Newsletter.jsx
@@ -3,35 +3,54 @@ import styled from 'styled-components'
 import SendIcon from '@mui/icons-material/Send';
 
 const Component = styled.div`
-    
+    height: 60vh;
+    background-color: #fcf5f5;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `
 
 const Title = styled.h1`
-
+    font-size: 3rem;
+    margin-bottom: 20px;
 `
 
 const Description = styled.p`
-    
+    margin-bottom: 20px;
 `
-const InputContainer = styled.div`
-    
-`
-const Input = styled.input`
 
+const InputContainer = styled.div`
+    width: 50%;
+    height: 40px;
+    background-color: white;
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid lightgray;
+    border-radius: 8px;
+`
+
+const Input = styled.input`
+    border: none;
+    flex: 8;
+    padding-left: 10px;
 `
 const Button = styled.button`
-
+    flex: 1;
+    border: none;
+    background-color: #73d9d9;
+    color: white;
 `
 
 const Newsletter = () => {
   return (
     <Component>
-        <Title>Newletter</Title>
+        <Title>Newsletter</Title>
         <Description>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque minima quisquam, voluptatem ratione aliquam corrupti accusamus itaque asperiores nemo suscipit.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit, corrupti.
         </Description>
         <InputContainer>
-            <Input />
+            <Input placeholder="Your Email..." />
             <Button>
                 <SendIcon />
             </Button>
