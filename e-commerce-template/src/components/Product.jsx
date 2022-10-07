b3b8d8f02abeb2ba36cc03648d37bbf431e521d8
Brian Garton <gartonb@gmail.com>
2022-10-07 10:18:09 +0200

    Info hover effected added over Container

--- e-commerce-template/src/components/Product.jsx
+++ e-commerce-template/src/components/Product.jsx
@@ -4,6 +4,22 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 
+const Info = styled.div`
+    opacity: 0;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-color: rgba(0,0,0,0.2);
+    z-index: 3;
+    cursor: pointer;
+    transition: all 0.5s ease;
+`;
+
 const Container = styled.div`
     flex: 1;
     margin: 5px;
@@ -14,6 +30,10 @@ const Container = styled.div`
     align-items: center;
     background-color: #f5fbfd;
     position: relative;
+
+    &:hover ${Info}{
+        opacity: 1;
+    }
 `
 
 const Circle = styled.div`
@@ -29,18 +49,7 @@ const Image = styled.img`
     object-fit: contain;
     z-index: 2;
 `
-const Info = styled.div`
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
-    display: flex;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(0,0,0,0.2);
-    z-index: 3;
-`
+
 const Icon = styled.div`
     display: flex;
     width: 40px;
@@ -52,11 +61,11 @@ const Icon = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
-    transition: all 0.5s ease;
+    transition: all 0.3s ease;
 
     &:hover{
         background-color: #e9f5f5;
-        transform: scale(1.1);
+        transform: scale(1.2);
     }
 `
 
