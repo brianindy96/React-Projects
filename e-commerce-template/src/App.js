500502d0554aa34642cd8806427f11fcb65e8f8d
Brian Garton <gartonb@gmail.com>
2022-10-08 07:17:00 +0200

    set Navbar components to ProductList.jsx

--- e-commerce-template/src/App.js
+++ e-commerce-template/src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import React from 'react';
-import Home from './pages/Home';
+// import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <ProductList />
     </div>
   );
 }
---
+++ e-commerce-template/src/pages/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from "styled-components";
+import Navbar from "../components/Navbar"
+const Container = styled.div`
+    
+`
+const ProductList = () => {
+  return (
+    <Container>
+        <Navbar />
+    </Container>
+  )
+}
+
+export default ProductList
