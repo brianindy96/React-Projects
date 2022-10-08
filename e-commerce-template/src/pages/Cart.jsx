0b823d96764d7d2fd4313de412827324e238fa5b
Brian Garton <gartonb@gmail.com>
2022-10-08 12:37:35 +0200

    Top done

--- e-commerce-template/src/pages/Cart.jsx
+++ e-commerce-template/src/pages/Cart.jsx
@@ -10,10 +10,54 @@ const Container = styled.div`
 
 const Wrapper = styled.div`
     min-height: 80vh;
+    padding: 20px;
 `
 
 const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+
+// --------------------TOP------------------------
+const Top = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 20px;
+`
+
+const TopButton = styled.button`
+    padding: 10px;
+    font-weight: 600;
+    border: ${(props)=> props.type === "filled" && "none"};
+    background-color:  ${(props) =>
+    props.type === "filled" ? "black" : "transparent"};
+    color: ${(props) => props.type === "filled" && "white"};
+`
+
+const TopTexts = styled.div`
     
+`
+
+const TopText = styled.span`
+    text-decoration: underline;
+    cursor: pointer;
+    margin: 0px 10px;
+`
+// ------------------------BOTTOM---------------------------
+
+const Bottom = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+`
+
+const Info = styled.div`
+    flex: 3;
+`
+
+const Summary = styled.div`
+    flex: 1;
 `
 
 const Cart = () => {
@@ -23,6 +67,18 @@ const Cart = () => {
         <Announcement />
         <Wrapper>
             <Title>Your Cart</Title>
+            <Top>
+                <TopButton>CONTINUE SHOPPING</TopButton>
+                <TopTexts>
+                    <TopText>Shopping Bag(2)</TopText>
+                    <TopText>Your Wishlist (0)</TopText>
+                </TopTexts>
+                <TopButton type="filled">CHECKOUT NOW</TopButton>
+            </Top>
+            <Bottom>
+                <Info>Info</Info>
+                <Summary>Summary</Summary>
+            </Bottom>
         </Wrapper>
         <Footer />
     </Container>
