de764c172341fcd9424f0a59cca2be84ef40c653
Brian Garton <gartonb@gmail.com>
2022-10-08 08:55:41 +0200

    starting with Filters

--- e-commerce-template/src/pages/Product.jsx
+++ e-commerce-template/src/pages/Product.jsx
@@ -10,27 +10,99 @@ const Container = styled.div`
 `
 
 const Wrapper = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    padding: 20px;
+`
+
+const ImageContainer = styled.div`
+    flex: 1;
+    min-width: 500px;
+    padding: 30px;
+`
+
+const Image = styled.img`
+    object-fit: cover;
+    max-height:100%; 
+    max-width:100%;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    margin-top: 20px;
+    padding: 0 50px;
+`
+
+const Title = styled.h1`
+    font-weight: 200;
+`
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 2rem;
+`
+
+// -------------------Filters------------------
+
+const FilterContainer = styled.div`
     
 `
 
-const Left = styled.div`
+const Filter = styled.div`
     
 `
-const Right = styled.div`
+
+const FilterTitle = styled.span`
     
 `
+
+const FilterColor = styled.div`
+    
+`
+const FilterSize = styled.select`
+
+`
+const FilterSizeOption = styled.option`
+    
+`
+
+
 const Product = () => {
   return (
     <Container>
         <Navbar />
         <Announcement />
         <Wrapper>
-            <Left>
+            <ImageContainer>
+                <Image src="/img/suit.jpg"/>
+            </ImageContainer>
+            <InfoContainer>
+                <Title>100% WOOL SUIT TROUSERS </Title>
+                <Desc>Italian wool trousers. Straight fit. Front pockets. Decorative pockets with double piping </Desc>
+                <Price>20,0 EUR</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Color</FilterTitle>
+                        <FilterColor color="black"/>
+                        <FilterColor color="blue"/>
+                        <FilterColor color="gray"/>
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Size</FilterTitle>
+                        <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
 
-            </Left>
-            <Right>
-
-            </Right>
+                </FilterContainer>
+            </InfoContainer>
         </Wrapper>
         <Newsletter />
         <Footer />
