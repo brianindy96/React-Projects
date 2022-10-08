039fc999c08d40e2843dcecb846584c13430309b
Brian Garton <gartonb@gmail.com>
2022-10-08 07:39:22 +0200

    Filter and Sort By with Select and Options added

--- e-commerce-template/src/pages/ProductList.jsx
+++ e-commerce-template/src/pages/ProductList.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import styled from "styled-components";
 import Navbar from "../components/Navbar"
 import Announcement from "../components/Announcement"
+import Products from "../components/Products"
+import Newsletter from "../components/Newsletter"
+import Footer from "../components/Footer"
 
 const Container = styled.div`
     
@@ -17,6 +20,21 @@ const FilterContainer = styled.div`
   margin: 0 20px;
 `
 const Filter = styled.div`
+  margin: 20px;
+`
+
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 500;
+  margin-right: 20px;
+`
+
+const Select = styled.select`
+  margin-right: 10px;
+  padding: 10px;
+`
+
+const Option = styled.option`
   
 `
 const ProductList = () => {
@@ -27,12 +45,42 @@ const ProductList = () => {
         <Title>Dresses</Title>
         <FilterContainer>
           <Filter>
-            Filter 1
+            <FilterText>Filter Product:</FilterText>
+            <Select>
+              <Option disabled selected>
+                Color
+              </Option>
+              <Option>White</Option>
+              <Option>Black</Option>
+              <Option>Red</Option>
+              <Option>Blue</Option>
+              <Option>Yellow</Option>
+              <Option>Green</Option>
+            </Select>
+            <Select>
+              <Option disabled selected>
+                Size
+              </Option>
+              <Option>XS</Option>
+              <Option>S</Option>
+              <Option>M</Option>
+              <Option>L</Option>
+              <Option>XL</Option>
+            </Select>
           </Filter>
           <Filter>
-            Filter 2
+            <FilterText>Sort Product:</FilterText>
+            <Select>
+              <Option selected>Bestseller</Option>
+              <Option>Newest</Option>
+              <Option>Price (lowest to highest)</Option>
+              <Option>Price (highest to lowest)</Option>
+            </Select>
           </Filter>
         </FilterContainer>
+        <Products />
+        <Newsletter />
+        <Footer />
     </Container>
   )
 }
