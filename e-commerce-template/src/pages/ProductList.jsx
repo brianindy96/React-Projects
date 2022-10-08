05cf62949737ee9eeeb4c4969db628bb79d8b3da
Brian Garton <gartonb@gmail.com>
2022-10-08 07:22:27 +0200

    Title and FilterContainer

--- e-commerce-template/src/pages/ProductList.jsx
+++ e-commerce-template/src/pages/ProductList.jsx
@@ -1,13 +1,38 @@
 import React from 'react'
 import styled from "styled-components";
 import Navbar from "../components/Navbar"
+import Announcement from "../components/Announcement"
+
 const Container = styled.div`
     
+`
+
+const Title = styled.h1`
+  margin: 20px;
+`
+
+const FilterContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 0 20px;
+`
+const Filter = styled.div`
+  
 `
 const ProductList = () => {
   return (
     <Container>
         <Navbar />
+        <Announcement />
+        <Title>Dresses</Title>
+        <FilterContainer>
+          <Filter>
+            Filter 1
+          </Filter>
+          <Filter>
+            Filter 2
+          </Filter>
+        </FilterContainer>
     </Container>
   )
 }
