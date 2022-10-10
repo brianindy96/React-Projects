6b6077a9c84b1d441b7e6743bef2cd0918bb31d5
Brian Garton <gartonb@gmail.com>
2022-10-10 11:18:39 +0200

    thunk_action_creator

--- github-search-app/src/actions/fetchAction.js
+++ github-search-app/src/actions/fetchAction.js
@@ -20,3 +20,23 @@ export const receive_error = () =>{
         type: "RECEIVE_ERROR"
     }
 }
+
+export const thunk_action_creator = username => {
+    // removes white space of username 
+    const user = username.replace(/\s/g, "")
+    store.dispatch(fetch_post());
+    // onced dispatched, executes the following function
+    return function(dispatch, getState){
+        return fetch(`https://api.github.com/users/${user}`)
+            //fetched data and convert into json 
+            .then(data => data.json())
+            // check conditions of the data's json
+            .then(data => {
+                if(data.message === "Not Found"){
+                    throw new Error("No such user found!");
+                } else dispatch(receive_post(data))
+            })
+            // if there is any error; dispatch(receive_error)
+            .catch(err => dispatch(receive_error));
+    }
+}
--- github-search-app/src/reducers/index.js
+++ github-search-app/src/reducers/index.js
@@ -4,8 +4,15 @@ const initialState = {
     isError: false,
 }
 
+// PURE FUNCTION : cannot works as asynchronous
 const asyncReducer = (state=initialState, action) =>{
     return state;
 }
+
+//THUNK:
+// when action is dispatched
+// the store receives the the action as an Object
+// redux-thunk changes it to function 
+// TEMPLATE: return function(dispatch, getState)
 
 export default asyncReducer;
