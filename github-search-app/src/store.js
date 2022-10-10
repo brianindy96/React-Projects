7766b665285965b2e6af9984164a447c2c087587
Brian Garton <gartonb@gmail.com>
2022-10-10 11:04:54 +0200

    initialState and asyncReducer created

--- github-search-app/src/store.js
+++ github-search-app/src/store.js
@@ -5,6 +5,8 @@ import thunk from "redux-thunk"
 // create store that accesses the asynchronous reducers
 const store = createStore(
     asyncReducer,
+    // intercepts action and check conditions
+    // before sending it to store
     applyMiddleware(thunk)
 );
 
