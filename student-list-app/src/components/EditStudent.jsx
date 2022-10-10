ac3629b70533f0666e982f898870cf0846891ad6
Brian Garton <gartonb@gmail.com>
2022-10-10 09:02:10 +0200

    done

--- student-list-app/src/components/EditStudent.jsx
+++ student-list-app/src/components/EditStudent.jsx
@@ -1,13 +1,96 @@
 import React, { Component } from 'react'
+import { connect } from "react-redux";
 
 export class EditStudent extends Component {
+    state = {
+        id: '',
+        name: '',
+        score: '',
+    }
+
+    onChangeData = (e) =>{
+        this.setState(
+            {
+                [e.target.name]: e.target.value
+            }
+        )
+    }
+    componentDidMount(){
+        const userId = this.props.match.params.id;
+        const student = this.getCurrentStudent(userId);
+        if(student){
+            this.setState({
+                id: student.id,
+                name: student.name,
+                score: student.score,
+            })
+        } else{
+            this.props.history.push('/');
+        }
+    }
+    getCurrentStudent(id){
+        const allStudents = this.props.studentsFromStore;
+        const [student] = allStudents.filter((item) =>{
+            return item.id === id;
+        });
+        return student;
+    }
+    onSubmitStudentForm = (data, e) =>{
+        e.preventDefault();
+        const newData ={
+            id: data.id,
+            name: data.name,
+            score: data.score,
+        }
+        this.props.editStudentAtStore(newData);
+        this.props.history.push('/');
+    }
   render() {
     return (
-      <div>
-        
+        <div className="card-body">
+        <form onSubmit={this.onSubmitStudentForm.bind(this, this.state)}>
+          <div className="form-group">
+            <label htmlFor="">Name</label>
+            <input 
+            type="text"
+            className="form-control"
+            name="name"
+            value={this.state.name}
+            onChange={this.onChangeData}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="">Score</label>
+            <input 
+            type="text"
+            className="form-control"
+            name="score"
+            value={this.state.score}
+            onChange={this.onChangeData}
+            />
+          </div>
+          <div className="form-group">
+            <button onClick={() => this.props.history.push('/')}>Cancel</button>
+            <button type="submit">Edit</button>
+          </div>
+        </form>
       </div>
     )
-  }
+    }
 }
 
-export default EditStudent
+const mapStateToProps = state => {
+    return {
+        studentsFromStore: state.students
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        editStudentAtStore : (id) => {
+            return dispatch({type: 'EDIT_STUDENT', payload: id})
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditStudent);
