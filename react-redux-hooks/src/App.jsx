8cf3e8a4b3e247f2c814feedef634585513a3d63
Brian Garton <gartonb@gmail.com>
2022-10-10 09:50:26 +0200

    useSelector in App.js imported

--- react-redux-hooks/src/App.jsx
+++ react-redux-hooks/src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+
+import { useSelector } from 'react-redux'
 import './App.css'
 
 function App() {
   
+  // state inside the store
+  // useSelector is a callback function
+  // returning the state 
+  const state = useSelector((state) => state)
+  // OUTPUT: Object {account:0}
+  
+  //   const state = useSelector((state) => state.account)
+  //  OUTPUT: 0
+  console.log(state);
   return (
     <div className="App">
       
