80fc2aad1faf10f37dbadd7438a514026a4fe3d8
Brian Garton <gartonb@gmail.com>
2022-10-10 11:30:19 +0200

    App.jsx layout and handleSubmit

--- github-search-app/src/App.jsx
+++ github-search-app/src/App.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // variable that holds the input value 
+  let getUsername;
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const username = getUsername.value;
+    console.log(username);
+    
+  }
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="container">
+      <form 
+      onSubmit={handleSubmit}
+      className="form"
+      >
+        <h2 className="title">Enter the Github Username</h2>
+        <input 
+        type="text"
+        placeholder="Enter Github Username"
+        required
+        ref={input => (getUsername = input)}
+         />
+        <button type="submit" className="button">Search</button>
+      </form>
+      
     </div>
   )
 }
--- github-search-app/src/store.js
+++ github-search-app/src/store.js
@@ -10,4 +10,4 @@ const store = createStore(
     applyMiddleware(thunk)
 );
 
-export default store;
+export { store };
