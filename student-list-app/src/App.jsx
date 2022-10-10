60fc4b04605942e5e4e01358ba120635dcb55297
Brian Garton <gartonb@gmail.com>
2022-10-10 08:01:48 +0200

    bring in Provider to pass in store as props

--- student-list-app/src/App.jsx
+++ student-list-app/src/App.jsx
@@ -2,20 +2,32 @@ import './App.css'
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Header from './components/Header'
 import EditStudent from './components/EditStudent'
+import { createStore } from "redux";
+import reducer from './reducer';
+
+// now uses Provider to let us use the store in the App
+import { Provider } from "react-redux"
+
 function App() {
+
+  // create a store and lets the reducer access the store
+  const store = createStore(reducer);
   
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Switch>
-          <Route exact path="/edit/:id" component={EditStudent} />
-          <Route exact path="/add" component={AddStudent} />
-          <Route exact path="/" component={StudentLists} />
-          <Route component={PageNotFound} />
-        </Switch>
-      </BrowserRouter>
-    </div>
+    // to let Provider bring store in, pass as props
+    <Provider store={store}>
+      <div className="App">
+        <BrowserRouter>
+          <Header />
+          <Switch>
+            <Route exact path="/edit/:id" component={EditStudent} />
+            <Route exact path="/add" component={AddStudent} />
+            <Route exact path="/" component={StudentLists} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    </Provider>
   )
 }
 
