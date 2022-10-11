d5418b1500063a9d92662f7d32d1a53dcb03026f
Brian Garton <gartonb@gmail.com>
2022-10-11 15:13:41 +0200

    select items before setting State

--- test-ecom/src/components/AddressForm.jsx
+++ test-ecom/src/components/AddressForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useForm, FormProvider } from "react-hook-form";
 import styled from "styled-components";
 import FormInput from './FormInput';
+import { Select, SelectChangeEvent, MenuItem} from "@mui/material";
 
 const Container = styled.div`
     
@@ -14,13 +15,22 @@ const Form = styled.form`
 
 `
 
-const FormDiv = styled.div`
+const BasicDetails = styled.div`
     width: 100%;
     display: grid;
     margin-left: 10px;
     grid-template-columns: repeat(auto-fill, minmax(200px, 450px));
     grid-gap: 25px;
 `
+
+const SelectCon = styled.div`
+  
+`
+
+const InputLabel = styled.h3`
+  
+`
+
 const AddressForm = () => {
     // gives us all the methods from react-hook-form to use
     const methods = useForm();
@@ -35,14 +45,38 @@ const AddressForm = () => {
         {/* spreads all the method from react-hook-form */}
         <FormProvider {...methods}>
             <Form onSubmit={onSubmit}>
-              <FormDiv>
+              <BasicDetails>
                 <FormInput required name="firstName" label="First Name" />         
                 <FormInput required name="lastName" label="Last Name"/>          
                 <FormInput required name="address" label="Address" />          
                 <FormInput required name="email" label="Email" />           
                 <FormInput required name="city" label="City" />
                 <FormInput required name="zip" label="ZIP/Postal Code" />
-              </FormDiv>
+              </BasicDetails>
+              <SelectCon>
+                <InputLabel>Shipping Country</InputLabel>
+                <Select value='' fullWidth onChange=''>
+                  <MenuItem key='' value=''>
+                    Select Me
+                  </MenuItem>
+                </Select>
+              </SelectCon>
+              <SelectCon>
+                <InputLabel>Shipping Subdivision</InputLabel>
+                <Select value='' fullWidth onChange=''>
+                  <MenuItem key='' value=''>
+                    Select Me
+                  </MenuItem>
+                </Select>
+              </SelectCon>
+              <SelectCon>
+                <InputLabel>Shipping City</InputLabel>
+                <Select value='' fullWidth onChange=''>
+                  <MenuItem key='' value=''>
+                    Select Me
+                  </MenuItem>
+                </Select>
+              </SelectCon>
             </Form>
         </FormProvider>
     </Container>
