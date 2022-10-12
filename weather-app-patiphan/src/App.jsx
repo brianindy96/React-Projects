a3b5e64607e8f792230741a2b9d8b4ab37f4c1f4
Brian Garton <gartonb@gmail.com>
2022-10-12 17:09:51 +0200

    simple layout and UI

--- weather-app-patiphan/src/App.jsx
+++ weather-app-patiphan/src/App.jsx
@@ -1,11 +1,51 @@
 import './App.css'
 import { useState } from "react";
-import keys from "./keys";
+// import keys from "./keys";
+
+// const api = {
+//   key: keys.API_KEY,
+//   base: keys.BASE_URL,
+// };
 
 function App() {
+
+ const dataBuild = (d) => {
+  let date = String(new window.Date());
+  // "Wed Oct 12 2022 12:49:24 GMT+0200 (Central European Summer Time)"
+  date = date.slice(3, 15);
+  // Oct 12 2022 :: STILL STRING
+  return date;
+}
+
   return (
-    <div className="App">
-      
+    <div className="App cloudy">
+      <main>
+        <div className="search-container">
+          <input 
+          type="text" 
+          placeholder='Search...'
+          className='search-bar'
+          />
+        </div>
+        <div>
+          <div className="location-container">
+            <div className="location">
+              Bangkok, Thailand
+            </div>
+            <div className="date">
+              Oct 12 2022
+            </div>
+            <div className='weather-container'>
+              <div className="temperature">
+                30 °C
+              </div>
+              <div className="weather">
+                Sunny
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   )
 }
--- weather-app-patiphan/src/keys.js
+++ weather-app-patiphan/src/keys.js
@@ -2,3 +2,4 @@ module.exports= {
     API_KEY: "0dbec0d676a684832a0e83a3f151f75d",
     BASE_URL: "https://api.openweathermap.org/data/2.5/"
 }
+
