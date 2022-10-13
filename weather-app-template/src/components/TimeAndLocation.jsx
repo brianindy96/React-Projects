fa37d71cb17e227982a29133b010f409c87024c5
Brian Garton <gartonb@gmail.com>
2022-10-13 11:17:41 +0200

    TimeAndLocation UI

---
+++ weather-app-template/src/components/TimeAndLocation.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const TimeAndLocation = () => {
+  return (
+    <div>
+        <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          13th October 2022, 11:13 Uhr
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">Berlin, Deutschland</p>
+      </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation
