53aa6e4100ef851834e5a99c039f02eed0500bbb
Brian Garton <gartonb@gmail.com>
2022-10-15 17:59:23 +0200

    Location Query and handleSearch Query done

--- weather-app-template/src/components/Inputs.jsx
+++ weather-app-template/src/components/Inputs.jsx
@@ -1,23 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PlaceIcon from '@mui/icons-material/Place';
 import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from '@mui/material';
 
-const Inputs = () => {
+const Inputs = ({ setQuery, units, setUnits}) => {
+
+  const [city, setCity] = useState('');
+  
+  const handleSearch = (e) =>{
+    e.preventDefault();
+
+    if(city !== '') setQuery({q: city})
+    
+    setCity("");
+  }
+
+  const handleLocationClick = () =>{
+    // if we have permission to user's location
+    if(navigator.geolocation){
+      
+      navigator.geolocation.getCurrentPosition((position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+
+        setQuery({
+          lat,
+          lon
+        })
+      })
+    }
+  }
+
   return (
     <div className='flex flex-row justify-center my-6'>
         <div className="flex flex-row w-3/4 items-center justify-center space-x-4" >
             <input 
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
             type="text"
             placeholder='Search your City here..' 
             className='text-xl font-light p-2 w-full shadow-xl focus:outline-none capitalize'
             />
-            
-            <SearchIcon 
-            style={{color: "white", cursor: "pointer"}}
-            className="text-white cursor-pointer transition ease-out hover:scale-110"
-            />
-
+            {console.log(city)}
+            <IconButton>
+              <SearchIcon 
+              onClick={handleSearch}
+              style={{color: "white", cursor: "pointer"}}
+              className="text-white cursor-pointer transition ease-out hover:scale-110"
+              />
+            </IconButton>
             <PlaceIcon 
+            onClick={handleLocationClick}
             style={{color: "white", cursor: "pointer"}}
             className="text-white cursor-pointer transition ease-out hover:scale-110"
             />
@@ -26,6 +59,7 @@ const Inputs = () => {
             <button
             name="metric"
             className="text-xl text-white font-light transition ease-out hover:scale-110"
+            // onClick={() => setUnits("metric")}
             >
             °C
             </button>
