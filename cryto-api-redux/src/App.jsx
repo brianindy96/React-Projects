6442bfee2f4d48b2f99abc604bf074ad95319440
Brian Garton <gartonb@gmail.com>
2022-10-16 11:14:23 +0200

    Footer done

--- cryto-api-redux/src/App.jsx
+++ cryto-api-redux/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navbar, Exchanges, HomePage, Cryptocurrencies, CryptoDetails, News } from './components';
 import { Layout, Typography, Space } from "antd";
+import { Link } from "react-router-dom";
 
 function App() {
 
@@ -22,9 +23,17 @@ function App() {
             </Routes>
           </div>
         </Layout>
-      </div>
-      <div className="footer">
-
+        <div className="footer">
+          <Typography.Title level={5} style={{color: 'white', textAlign: 'center'}}> 
+            Cryptoverse <br/> All rights reserved
+          </Typography.Title>
+          <Space>
+            <Link to="/">Home</Link>
+            <Link to="/exchanges">Exchanges</Link>
+            <Link to="/cryptocurrencies">Cryptocurrencies</Link>
+            <Link to="/news">News</Link>
+          </Space>
+        </div>
       </div>
     </div>
   )
