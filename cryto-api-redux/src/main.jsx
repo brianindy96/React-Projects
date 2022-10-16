fdceb55ba48acd8838fbc5e16686cda335312a6e
Brian Garton <gartonb@gmail.com>
2022-10-16 11:39:45 +0200

    set up store.js, Provider

--- cryto-api-redux/src/main.jsx
+++ cryto-api-redux/src/main.jsx
@@ -4,11 +4,15 @@ import App from './App'
 import './index.css'
 import { BrowserRouter, BrowserRouter as Router } from "react-router-dom"
 import 'antd/dist/antd.css' 
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>
 )
