7ff43776f618cf627c82395fc2f8ca50c47a2b07
Brian Garton <gartonb@gmail.com>
2022-10-20 13:35:57 +0200

    dynamic Balance.jsx

--- expense-tracker/src/components/Balance.jsx
+++ expense-tracker/src/components/Balance.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 const Balance = () => {
+
+  const { transactions } = useContext(GlobalContext);
+  const amounts = transactions.map((transaction) => transaction.amount)
+  const total = amounts.reduce((a, c) => a += c, 0).toFixed(2);
+  
+  // console.log(amounts);
+  // => array of transactions.amounts
+
+  // console.log(total);
+  // => total sum of those amounts
   return (
     <>
-      <h4>Your Balance: </h4> 
-      <h1>€0,00</h1> 
+      <h4>Your Balance:</h4> 
+      <h1> €{total}</h1> 
     </>
   )
 }
