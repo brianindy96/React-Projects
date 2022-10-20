9c36cbdd1e5eeca395df0f02f63d71e3e5a2863d
Brian Garton <gartonb@gmail.com>
2022-10-20 13:16:06 +0200

    imported GlobalContext to TransactionsList.jsx and map through transactions

--- expense-tracker/src/components/TransactionList.jsx
+++ expense-tracker/src/components/TransactionList.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
+import { useContext } from 'react'
+// import GlobalContext from GlobalState.jsx
+import { GlobalContext } from '../context/GlobalState'
 
 const TransactionList = () => {
+
+  // returns the Object
+  // const context = useContext(GlobalContext);
+
+  // returns array
+  const { transactions } = useContext(GlobalContext);
+
   return (
     <>
         <h3>History</h3>
         <ul className="list">
-            <li className="minus">
-                Cash <span>-€400</span><button className="delete-btn">x</button>
+            {transactions.map((transaction) => (
+              <li key={transaction.id} className="minus">
+                {transaction.text} <span>€{transaction.amount.toFixed(2)}</span><button className="delete-btn">x</button>
             </li>
+            ))}
         </ul>
     </>
   )
