9d90f5f589b1793484c032367c48ce6d29a11ff7
Brian Garton <gartonb@gmail.com>
2022-10-20 13:28:08 +0200

    const sign fixed with Math.abs on transaction amount

--- expense-tracker/src/components/Transaction.jsx
+++ expense-tracker/src/components/Transaction.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Transaction = ({ transaction, amount }) => {
-  return (
-    <li className={transaction.amount < 0 ? "minus" : "plus"}>
-        {transaction.text} <span>€{transaction.amount.toFixed(2)}</span><button className="delete-btn">x</button>
-    </li>
-  )
+const Transaction = ({ transaction }) => {
+
+    const sign = transaction.amount < 0 ? "-" : "+";
+
+    return (
+        <li className={sign < 0 ? "minus" : "plus"}>
+            {transaction.text} <span>{sign + " "}€{Math.abs(transaction.amount.toFixed(2))}</span><button className="delete-btn">x</button>
+        </li>
+    )
 }
 
 export default Transaction
