3fe9a1a24263802e486142f5b336b38f15652070
Brian Garton <gartonb@gmail.com>
2022-10-21 12:53:34 +0200

    projectId and handleLogout with chat Engine UI

--- chat-app-firebase-context/src/components/Chats.jsx
+++ chat-app-firebase-context/src/components/Chats.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { ChatEngine } from "react-chat-engine"
+import { auth } from "../firebase";
+import { useAuth } from '../context/AuthContext';
 
 const Chats = () => {
+
+    const projectId = import.meta.env.VITE_PROJECT_ID;
+    const navigate = useNavigate();
+    const { user } = useAuth();
+    // console.log(user);
+    const handleLogout = async () => {
+        await auth.signOut();
+
+        navigate('/');
+    }
+    
+
   return (
-    <div>Chats</div>
+    <div className="chats-page">
+        <div 
+        className="nav-bar"
+        onClick={handleLogout}
+        >
+            <div className="logo-tab">
+                ChatApp
+            </div>
+            <div className="logout-tab">
+                Logout
+            </div>
+        </div>
+            <ChatEngine 
+                height="calc(100vh-66px)"
+                projectId={projectId}
+                userName="."
+                userSecret="."
+            />
+    </div>
   )
 }
 
