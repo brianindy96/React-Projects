c2e0602ecac2084ae676685e90db3187384a2c38
Brian Garton <gartonb@gmail.com>
2022-10-21 11:05:36 +0200

    created and exported Login.jsx

--- chat-app-firebase-context/src/App.jsx
+++ chat-app-firebase-context/src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
   
   return (
     <div className="App">
-      
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
     </div>
   )
 }
