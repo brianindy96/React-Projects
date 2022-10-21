c01bf6393928aaa4d0cdf2bb16d4d3f75b8bbcce
Brian Garton <gartonb@gmail.com>
2022-10-21 11:10:45 +0200

    login UI done

--- chat-app-firebase-context/src/components/Login.jsx
+++ chat-app-firebase-context/src/components/Login.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import GoogleIcon from '@mui/icons-material/Google';
 
 const Login = () => {
   return (
-    <div>
-        
+    <div id='login-page'>
+        <div id="login-card">
+            <h2>Welcome to Chat!</h2>
+
+            <div className='login-button google'>
+                <GoogleIcon /> Login with Google
+            </div>
+            <br /><br/>
+            <div className='login-button facebook'>
+                <FacebookOutlinedIcon /> Login with Facebook
+            </div>
+        </div>
     </div>
   )
 }
