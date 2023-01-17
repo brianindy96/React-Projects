ac4f005c0928fc6e1eb15d3db462abec93f3a7ab
Brian Garton <gartonb@gmail.com>
2023-01-17 16:11:03 +0100

    installed styled-components and Navbar.jsx styled

--- e-com-template/src/components/Navbar.jsx
+++ e-com-template/src/components/Navbar.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  height: 60px;
+  background-color: black;
+  color: white;
+  text-align: center;
+  padding-top: 10px;
+`
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <Container>
+      Navbar
+    </Container>
   )
 }
 
