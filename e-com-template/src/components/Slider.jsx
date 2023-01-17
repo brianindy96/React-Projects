d6cd47928ea0b5fcec2f862021c8434743ce69c8
Brian Garton <gartonb@gmail.com>
2023-01-17 22:30:23 +0100

    Slider complete transform with translateX related to props

--- e-com-template/src/components/Slider.jsx
+++ e-com-template/src/components/Slider.jsx
@@ -1,4 +1,4 @@
-// import {useState} from 'react'
+import {useState} from 'react'
 import styled from "styled-components";
 import KeyboardArrowLeftOutlinedIcon from '@mui/icons-material/KeyboardArrowLeftOutlined';
 import KeyboardArrowRightOutlinedIcon from '@mui/icons-material/KeyboardArrowRightOutlined';
@@ -37,7 +37,7 @@ const Arrow = styled.div`
 const Wrapper = styled.div`
     height: 100%;
     display: flex;
-    transform: translateX(0vw)
+    transform: translateX(${props=> props.slideIndex * -100 }vw);
 `
 
 const Slide = styled.div`
@@ -80,15 +80,22 @@ const Button = styled.button`
 `
 
 const Slider = () => {
-    /* const [slideIndex, setSlideIndex] = useState(0); */
+    const [slideIndex, setSlideIndex] = useState(0);
 
-    const handleClick = (direction) => {};
+    const handleClick = (direction) => {
+        if(direction ==="left"){
+            setSlideIndex(slideIndex > 0 ? slideIndex -1 : 2);
+        } else{
+            setSlideIndex(slideIndex < 2 ? slideIndex +1: 0 )
+        };
+    };
+
   return (
     <Container>
         <Arrow direction="left" onClick={() => handleClick("left")}>
             <KeyboardArrowLeftOutlinedIcon/>
         </Arrow>
-        <Wrapper>
+        <Wrapper slideIndex={slideIndex}>
             {sliderItems.map((item)=>(
                 <Slide key={item.id} bg={item.bg}>
                     <ImgContainer>
