42a9b2c41a428b3bc509da7a45408159a2f2a60c
Brian Garton <gartonb@gmail.com>
2023-01-18 18:19:50 +0100

    Product.jsx page components added

--- e-com-template/src/pages/Product.jsx
+++ e-com-template/src/pages/Product.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import styled from "styled-components"
+import Announcement from '../components/Announcement'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+import Newsletter from '../components/Newsletter'
 
 const Container = styled.div`
     
@@ -7,7 +11,10 @@ const Container = styled.div`
 const Product = () => {
   return (
     <Container>
-        
+        <Navbar />
+        <Announcement />
+        <Newsletter />
+        <Footer />
     </Container>
   )
 }
