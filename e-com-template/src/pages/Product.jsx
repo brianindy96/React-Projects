89c1840d550b448d6d9eeb4e5c3fc4ebf2a5a514
Brian Garton <gartonb@gmail.com>
2023-01-18 18:30:24 +0100

    ImgContainer nad InfoContainer until Price

--- e-com-template/src/pages/Product.jsx
+++ e-com-template/src/pages/Product.jsx
@@ -8,11 +8,55 @@ import Newsletter from '../components/Newsletter'
 const Container = styled.div`
     
 `
+
+const Wrapper = styled.div`
+    padding: 50px;
+    display: flex;
+
+`
+
+const ImgContainer = styled.div`
+    flex: 1;
+`
+
+const Image = styled.img`
+    width: 100%;
+    height: 90vh;
+    object-fit: contain;
+`
+const InfoContainer = styled.div`
+    flex: 1;
+    padding: 0 50px;
+`
+
+const Title = styled.h1`
+    font-weight: 200;
+`
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`
+
+const Price = styled.span`
+    font-weight: 100;
+    font-size: 40px;
+`
+
 const Product = () => {
   return (
     <Container>
         <Navbar />
         <Announcement />
+        <Wrapper>
+            <ImgContainer>
+                <Image src="/img/suit.jpg" />
+            </ImgContainer>
+            <InfoContainer>
+                <Title>Denim Jumpsuit</Title>
+                <Desc>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam exercitationem nostrum eaque quaerat tenetur animi necessitatibus saepe ducimus magnam magni quo molestiae voluptate ipsa error incidunt harum fugit omnis tempora voluptatum, porro eveniet maxime deserunt consequatur? Ab fugiat nihil animi!</Desc>
+                <Price>$20</Price>
+            </InfoContainer>
+        </Wrapper>
         <Newsletter />
         <Footer />
     </Container>
