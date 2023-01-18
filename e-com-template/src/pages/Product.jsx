90a9384c2426c8cf81dc25a855e11fa1d2302c85
Brian Garton <gartonb@gmail.com>
2023-01-18 21:43:13 +0100

    Product details in SingleProductPage finished

--- e-com-template/src/pages/Product.jsx
+++ e-com-template/src/pages/Product.jsx
@@ -4,6 +4,8 @@ import Announcement from '../components/Announcement'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 import Newsletter from '../components/Newsletter'
+import RemoveIcon from '@mui/icons-material/Remove';
+import AddIcon from '@mui/icons-material/Add';
 
 const Container = styled.div`
     
@@ -42,6 +44,84 @@ const Price = styled.span`
     font-size: 40px;
 `
 
+const FilterContainer = styled.div`
+    display: flex;
+    margin: 30px 0px;
+    justify-content: space-between;
+    width: 50%;
+`
+
+const Filter = styled.div`
+    display: flex;
+    align-items: center;
+`
+
+const FilterTitle = styled.span`
+    font-size: 20px;
+    font-weight: 200;
+`
+
+const FilterColor = styled.div`
+    background-color: ${props=> props.color};
+    width: 20px;
+    height: 20px;
+    border-radius: 50%;
+    margin: 0 5px;
+    cursor: pointer;
+
+`
+
+const FilterSize = styled.select`
+    margin: 0 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 5px;
+    background-color: white;
+    border: 1px solid lightgrey;
+`
+
+const FilterSizeOption = styled.option`
+    
+`
+
+const AddContainer = styled.div`
+    display: flex;
+    align-items: center;
+    width: 50%;
+    justify-content: space-between;
+`
+
+const AmountContainer = styled.option`
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+`
+
+const Amount = styled.span`
+    width: 30px;
+    height: 30px;
+    border-radius: 10px;
+    border: 1px solid teal;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin: 0 5px;
+`
+const Button = styled.button`
+    padding: 15px;
+    border: 2px solid teal;
+    background-color: white;
+    font-weight: 500;
+    cursor: pointer;
+
+    &:hover{
+        background-color: #f8f4f4;
+    }
+`
+
+
+
 const Product = () => {
   return (
     <Container>
@@ -54,7 +134,34 @@ const Product = () => {
             <InfoContainer>
                 <Title>Denim Jumpsuit</Title>
                 <Desc>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam exercitationem nostrum eaque quaerat tenetur animi necessitatibus saepe ducimus magnam magni quo molestiae voluptate ipsa error incidunt harum fugit omnis tempora voluptatum, porro eveniet maxime deserunt consequatur? Ab fugiat nihil animi!</Desc>
-                <Price>$20</Price>
+                <Price>$ 20</Price>
+                <FilterContainer>
+                    <Filter>
+                        <FilterTitle>Colour:</FilterTitle>
+                        <FilterColor color="black" />
+                        <FilterColor color="red" />
+                        <FilterColor color="blue" />
+                        <FilterColor color="gray" />
+                    </Filter>
+                    <Filter>
+                        <FilterTitle>Size:</FilterTitle>
+                        <FilterSize>
+                            <FilterSizeOption>XS</FilterSizeOption>
+                            <FilterSizeOption>S</FilterSizeOption>
+                            <FilterSizeOption>M</FilterSizeOption>
+                            <FilterSizeOption>L</FilterSizeOption>
+                            <FilterSizeOption>XL</FilterSizeOption>
+                        </FilterSize>
+                    </Filter>
+                </FilterContainer>
+                <AddContainer>
+                    <AmountContainer>
+                        <RemoveIcon />
+                        <Amount> 1 </Amount>
+                        <AddIcon />
+                    </AmountContainer>
+                    <Button>ADD TO CART</Button>
+                </AddContainer>
             </InfoContainer>
         </Wrapper>
         <Newsletter />
