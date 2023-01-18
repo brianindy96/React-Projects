521ccd9531cdc69d673d4719cac65bc69a015c94
Brian Garton <gartonb@gmail.com>
2023-01-18 16:57:26 +0100

    Footer.jsx left

--- e-com-template/src/components/Footer.jsx
+++ e-com-template/src/components/Footer.jsx
@@ -1,25 +1,78 @@
 import React from 'react'
 import styled from "styled-components";
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
 
 const Container = styled.div`
     display: flex;
 `
 
+// Left
 const Left = styled.div`
     flex: 1;
+    padding: 20px;
+    display: flex;
+    flex-direction: column;
+
 `
+
+const Logo = styled.h1`
+    font-size: 40px;
+`
+
+const Desc = styled.p`
+    margin: 20px 0px;
+`
+const SocialContainer = styled.div`
+    display: flex;
+`
+
+const SocialIcon = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    color: white;
+    background-color: ${props=> props.color};
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 10px;
+    cursor: pointer;
+`
+
+// Center
+
 const Center = styled.div`
     flex: 1;
 `
+
+// Right
 const Right = styled.div`
     flex: 1;
 `
+
 
 const Footer = () => {
   return (
     <Container>
         <Left>
-            Left
+            <Logo>NOTINO.</Logo>
+            <Desc>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae eius numquam a voluptatibus ut et quaerat iste, accusamus sed dolorem?
+            </Desc>
+            <SocialContainer>
+                <SocialIcon color="#3B5999">
+                    <FacebookIcon />
+                </SocialIcon>
+                <SocialIcon color="#E4405F">
+                    <InstagramIcon />
+                </SocialIcon>
+                <SocialIcon color="#55ACEE">
+                    <TwitterIcon />
+                </SocialIcon>
+            </SocialContainer>
         </Left>
         <Center>
             Center
