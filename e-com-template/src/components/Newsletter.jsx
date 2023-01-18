f10cab245ef5357ffa2f3802c031bbe39ad4a42c
Brian Garton <gartonb@gmail.com>
2023-01-18 16:40:38 +0100

    Newsletter.jsx done

--- e-com-template/src/components/Newsletter.jsx
+++ e-com-template/src/components/Newsletter.jsx
@@ -12,28 +12,48 @@ const Container = styled.div`
 `
 
 const Title = styled.h1`
+    font-size: 70px;
+    margin-bottom: 20px;
 `
 
 const Description = styled.p`
-    margin: 10px;
+    font-size: 24px;
+    font-weight: 300;
+    margin-bottom: 20px;
 `
 
 const InputContainer = styled.div`
     display: flex;
-    justify-content: center;
+    justify-content: space-between;
     margin-top: 10px;
     align-items: center;
-    border: 1px solid black;
+    border: 1px solid lightgray;
+    width: 50%;
+    height: 40px;
+    background-color: white;
+
 `
 
 const Input = styled.input`
     padding: 5px;
+    flex: 8;
     border: none;
     outline: none;
+    padding-left: 20px;
 `
 
 const Button = styled.button`
-    
+    padding: 10px;
+    height: 100%;
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    cursor: pointer;
+    border: none;
+    background-color: teal;
+    color: white;
+
 `
 const Newsletter = () => {
   return (
@@ -42,8 +62,9 @@ const Newsletter = () => {
         <Description>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos, est.</Description>
         <InputContainer>
             <Input placeholder='Your email here....' />
-            <Button></Button>
-            <SendOutlinedIcon />
+            <Button>
+                <SendOutlinedIcon />
+            </Button>
         </InputContainer>
     </Container>
   )
