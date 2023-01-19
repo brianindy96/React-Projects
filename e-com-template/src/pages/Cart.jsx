bf8ca309c23d27492ac751457e5cc6c13405a70c
Brian Garton <gartonb@gmail.com>
2023-01-19 11:17:15 +0100

    Top and Bottom added; Title is styled

--- e-com-template/src/pages/Cart.jsx
+++ e-com-template/src/pages/Cart.jsx
@@ -8,7 +8,19 @@ const Container = styled.div`
     
 `
 const Wrapper = styled.div`
+    padding: 20px;
+`
+
+const Title = styled.h1`
+    font-weight: 300;
+    text-align: center;
+`
+
+const Top = styled.div`
     
+`
+
+const Bottom = styled.div`
 `
 
 const Cart = () => {
@@ -17,7 +29,13 @@ const Cart = () => {
         <Navbar />
         <Announcement />
         <Wrapper>
-            Cart
+            <Title>YOUR CART</Title>
+            <Top>
+
+            </Top>
+            <Bottom>
+
+            </Bottom>
         </Wrapper>
         <Footer />
     </Container>
