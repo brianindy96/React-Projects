94fb40feb67a19f1bc025a0e2ffdddc544f7318e
Brian Garton <gartonb@gmail.com>
2023-01-20 22:56:07 +0100

    Fetch products data from commerce

--- commerce-js-react/src/App.jsx
+++ commerce-js-react/src/App.jsx
@@ -2,15 +2,32 @@ import './App.css'
 import Products from './components/Products'
 import styled from 'styled-components'
 import Navbar from './components/Navbar'
+import commerce from "./lib/commerce"
+import { useState, useEffect } from "react"
 
 const Container = styled.div`
   
 `
 function App() {
+  const [products, setProducts ] = useState([]);
+
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
+
+    setProducts(data);
+
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, [])
+
+  console.log(products);
+  
   return (
     <Container>
       <Navbar />
-      <Products />
+      <Products products={products} />
     </Container>
   )
 }
