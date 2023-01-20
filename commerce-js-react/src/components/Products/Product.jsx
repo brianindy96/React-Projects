b95859237813e16d28061dc3d0a2e2b926817d59
Brian Garton <gartonb@gmail.com>
2023-01-20 12:59:32 +0100

    MockProducts and Card added

--- commerce-js-react/src/components/Products/Product.jsx
+++ commerce-js-react/src/components/Products/Product.jsx
@@ -1,25 +1,39 @@
 import React from 'react'
 import styled from "styled-components"
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const CardContainer = styled.div`
     width: 250px;
-    /* height: 500px; */ */
+    border: 1px solid lightgrey;
+    min-height: 350px;
+    margin: 10px;
+    border-radius: 25px;
+    padding: 10px;
+    background-color: #f8f8f8;
+    box-shadow: 8px 8px 17px -20px rgba(66, 68, 90, 1);
 `
 
 const CardTitle = styled.h2`
-    
+    font-size: 36px;
 `
 
 const ProductImg = styled.img`
-    
+    width: 150px;
 `
 
 const ProductDesc = styled.p`
-    
+    font-style: italic;
 `
 
 const ProductPrice = styled.p`
-    
+    font-size: 20px;
+    font-weight: 600;
+`
+
+const Icon = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    padding: 5px;
 `
 
 const Product = ({ product }) => {
@@ -29,6 +43,9 @@ const Product = ({ product }) => {
             <ProductImg src={product.img} alt="Product image here"/>
             <ProductDesc>{product.description}</ProductDesc>
             <ProductPrice>$ {product.price}</ProductPrice>
+            <Icon>
+                <AddShoppingCartIcon />
+            </Icon>
         </CardContainer>
   )
 }
--- commerce-js-react/src/components/Products/Products.jsx
+++ commerce-js-react/src/components/Products/Products.jsx
@@ -9,34 +9,47 @@ const products = [
         name: "Shoes",
         description: "running shoes",
         price: 30.00,
-        img: ""
+        img: "/img/shoe.png"
     },
     {
         id: 2,
-        name: "Shirt",
-        description: "normal shirt",
-        price: 15.00
+        name: "T-Shirt",
+        description: "normal t-shirt",
+        price: 15.00,
+        img: "/img/shirt.png"
     },
     {
       id: 3,
-      name: "T-Shirt",
-      description: "normal t-shirt",
-      price: 25.00
+      name: "Shirt",
+      description: "normal shirt",
+      price: 25.00,
+      img: "https://www.tuttu.pl/pol_pl_Koszula-JWP-LS-SHIRT-MEN-37094_4.png"
   },
 ]
 
 const Container = styled.div`
     width: 100vw;
     height: 100vh;
+    
+`
+
+const Wrapper = styled.div`
     display: grid;
     grid-template-columns: repeat(3,1fr);
+    text-align: center;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px 50px;
 `
 const Products = () => {
   return (
     <Container>
+      <Wrapper>
         {products.map((product)=>(
           <Product key={product.id} product={product} />
         ))}
+      </Wrapper>
+        
     </Container>
   )
 }
