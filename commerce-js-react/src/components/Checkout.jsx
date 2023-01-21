acc0b886514eb0bd1549987794e70b9fc8cbeb1c
Brian Garton <gartonb@gmail.com>
2023-01-21 21:07:34 +0100

    const Form and Confirmation in checkout.jsx

--- commerce-js-react/src/components/Checkout.jsx
+++ commerce-js-react/src/components/Checkout.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import styled from "styled-components"
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from "@mui/material"
+import AddressForm from "./AddressForm";
+import PaymentForm from "./PaymentForm";
+import Confirmation from "./Confirmation";
 
 const Container = styled.div`
   width: 100vw;
@@ -22,18 +25,33 @@ const Checkout = () => {
 
   const [activeStep, setActiveStep] = useState(0);
 
+  //Confirmation
+
+  const Confirmation = () => (
+    <div>
+      Confirmation
+    </div>
+  )
+
+
+  // Form component
+  const Form = () => activeStep === 0
+  ? <AddressForm />
+  : <PaymentForm />
+
   return (
     <Container>
       <Wrapper>
         <Paper style={{width: "50%", padding: "20px"}}>
           <Typography variant="h4" align="center">Checkout</Typography>
-          <Stepper activeStep={0}>
+          <Stepper activeStep={activeStep} style={{marginBottom: "20px"}}>
             {steps.map((step) => (
               <Step key={step}>
                 <StepLabel>{step}</StepLabel>
               </Step>
             ))}
           </Stepper>
+          {activeStep === steps.length ? <Confirmation /> : <Form />}
         </Paper>
       </Wrapper>
     </Container>
