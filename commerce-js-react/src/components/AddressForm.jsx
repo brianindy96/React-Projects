ef8f13e58cdf6f16f7c5d42a85f0e44d8b3846ba
Brian Garton <gartonb@gmail.com>
2023-01-21 21:16:54 +0100

    FormInput.jsx

--- commerce-js-react/src/components/AddressForm.jsx
+++ commerce-js-react/src/components/AddressForm.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import styled from "styled-components"
+import { InputLabel, Select, MenuItem, Button, Grid, Typography } from "@mui/material"
+import { useForm, FormProvider } from "react-hook-form"
+
+
+const Container = styled.div`
+    
+`
 
 const AddressForm = () => {
+
+    const methods = useForm();
+
+
   return (
-    <div>AddressForm</div>
+    <Container>
+        <Typography variant="h6" gutterBottom>Shipping Address</Typography>
+        <FormProvider {...methods}>
+            <form onSubmit={""}>
+                <Grid container spacing={3}>
+
+                </Grid>
+            </form>
+        </FormProvider>
+    </Container>
   )
 }
 
