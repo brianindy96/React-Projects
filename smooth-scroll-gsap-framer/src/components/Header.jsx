95811ff24cbdef308e362eed0346ed6a889b82e3
Brian Garton <gartonb@gmail.com>
2023-01-22 21:46:32 +0100

    Bai Jamjuree font imported

--- smooth-scroll-gsap-framer/src/components/Header.jsx
+++ smooth-scroll-gsap-framer/src/components/Header.jsx
@@ -1,11 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
 
+const Container = styled.div`
+    position: relative;
+`
+
+const HeaderList = styled.ul`
+    position: absolute;
+    left: 0;
+    top: 100px;
+
+    letter-spacing: 1px;
+    font-family: "Syncopate", sans-serif;
+    color: #626262;
+    text-transform: uppercase;
+`
+
+const HeaderListItem = styled.li`
+    margin: 10px 0;
+    text-decoration: none;
+`
+
+const Title = styled.h1`
+    font-size: 20vw;
+    text-transform: uppercase;
+    text-align: center;
+    font-weight: 600;
+    font-family: "Bai Jamjuree", sans-serif;
+`
+
+
+
 const Header = () => {
   return (
-    <div>
-        
-    </div>
+    <Container>
+        <HeaderList>
+            <HeaderListItem>Intro</HeaderListItem>
+            <HeaderListItem>About</HeaderListItem>
+            <HeaderListItem>Featured</HeaderListItem>
+        </HeaderList>
+        <Title>Art Objects</Title>
+    </Container>
   )
 }
 
