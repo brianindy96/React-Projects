56959c4b98531420aa731eb9035be628e2213ea7
Brian Garton <gartonb@gmail.com>
2023-01-22 21:34:18 +0100

    all components imported in App.jsx

--- smooth-scroll-gsap-framer/src/App.jsx
+++ smooth-scroll-gsap-framer/src/App.jsx
@@ -1,12 +1,32 @@
 // import { useState } from 'react'
 import './App.css'
+import About from './components/About'
+import CustomCursor from './components/CustomCursor'
+import Featured from './components/Featured'
+import Footer from './components/Footer'
+import Gallery from './components/Gallery'
+import Header from './components/Header'
+import Navbar from './components/Navbar'
 
 function App() {
 
+  const Container = styled.div`
+    
+  `
+
   return (
-    <div className="App">
-      Hi
-    </div>
+    <>
+      <CustomCursor />
+
+      <Container>
+        <Navbar />
+        <Header />
+        <Featured />
+        <About />
+        <Gallery />
+        <Footer />
+      </Container>
+    </>
   )
 }
 
