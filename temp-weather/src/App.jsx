74bb0c9c956a7bed794cbfc774265727dcd0f2d9
Brian Garton <gartonb@gmail.com>
2023-02-09 12:48:19 +0100

    fetchWeather() and weatherServices.jsx

--- temp-weather/src/App.jsx
+++ temp-weather/src/App.jsx
@@ -1,13 +1,25 @@
-import { AcUnit } from '@mui/icons-material'
 import './App.css'
 import Forecast from './components/Forecast'
 import Input from './components/Input'
 import Nav from './components/Nav'
 import TemperatureAndDetails from './components/TemperatureAndDetails'
 import TimeAndLocation from './components/TimeAndLocation'
+import { useEffect } from 'react'
+import getWeatherData from './services/weatherService'
 
 function App() {
 
+  // Fetch Weather
+  const fetchWeather = async () => {
+    const data = await getWeatherData("weather", {q: "london"});
+    console.log(data);
+  }
+
+  // useEffect
+  useEffect(() => {
+    fetchWeather();
+  }, [])
+  
   return (
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-28 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl">
       <Nav />
