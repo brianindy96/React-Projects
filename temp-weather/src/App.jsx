fb0274d30a37d73b61774f8ff107f50dbdc6d7c9
Brian Garton <gartonb@gmail.com>
2023-02-09 11:04:52 +0100

    TimeAndLocation.jsx first styled

--- temp-weather/src/App.jsx
+++ temp-weather/src/App.jsx
@@ -2,6 +2,7 @@ import { AcUnit } from '@mui/icons-material'
 import './App.css'
 import Input from './components/Input'
 import Nav from './components/Nav'
+import TimeAndLocation from './components/TimeAndLocation'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className="mx-auto max-w-screen-md mt-4 py-5 px-28 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl">
       <Nav />
       <Input />
+      <TimeAndLocation />
     </div>
   )
 }
---
+++ temp-weather/src/components/TimeAndLocation.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const TimeAndLocation = () => {
+  return (
+    <div>
+        <div className="flex items-center justify-center my-6">
+        <p className="text-white text-xl font-extralight">
+          Date and Time
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">
+          Berlin, Germany
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default TimeAndLocation
