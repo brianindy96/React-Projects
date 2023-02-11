3aaa8b624b60d17b455aa9ae0feefffcac78dfbc
Brian Garton <gartonb@gmail.com>
2023-02-11 22:23:43 +0100

    ArrowIcon for trigger pull up and background-color changed for Forecasts.jsx

--- weather-app/src/components/Forecasts.jsx
+++ weather-app/src/components/Forecasts.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import styled from 'styled-components'
 import Forecast from './Forecast'
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 
 const Container = styled.div`
-    background-color: white;
+    background-color: #e6e6e6;
     color: black;
     height: 60vh;
-    /* when show, Y(-170px), when hide Y(110px) */
-    transform: translateY(-170px);
+    /* when show, Y(-170px), when hide Y(180px) */
+    transform: translateY(-160px);
 `
+
+const Arrow = styled(KeyboardArrowUpIcon)`
+  cursor: pointer;
+  margin: 0;
+`
+
 
 
 const ForecastCon = styled.div`
@@ -16,13 +23,15 @@ const ForecastCon = styled.div`
   flex-direction: column;
 `
 
-const Title = styled.h3`
+const Title = styled.div`
   margin: 0;
 `
 const Forecasts = ({ weather }) => {
   return (
     <Container>
-        <Title>Title</Title>
+        <Title>
+          <Arrow />
+        </Title>
         <ForecastCon>
           <Forecast weather={weather} forecast={weather.today}/>
           <Forecast weather={weather} forecast={weather.tmr}/>
