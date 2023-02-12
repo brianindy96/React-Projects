e740b7612382e698c6d8ed3a2e0f7e6f25b6e505
Brian Garton <gartonb@gmail.com>
2023-02-12 21:03:29 +0100

    CardComponent basic styling

--- redux-toolkit-react-temp/src/components/MovieCard.jsx
+++ redux-toolkit-react-temp/src/components/MovieCard.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-`
+    border: 1px solid #ddd;
+    padding: 1rem;
+    border-radius: 10px;
+`   
 
 const MovieTitle = styled.h4`
     
@@ -13,11 +16,21 @@ const MovieRelease = styled.p`
 `
 
 const MovieImg = styled.img`
-    
+    width: 100%;
 `
 
 const ImgCon = styled.div`
     
+`
+
+const ReadMore = styled.a`
+    background-color: #333;
+    color: white;
+    text-decoration: none;
+    padding: 0.5rem;
+    display: inline-block;
+    margin-top: 0.5rem;
+    border-radius: 5px;
 `
 const MovieCard = ({ movie: {Title, Year, Poster} }) => {
   return (
@@ -28,6 +41,7 @@ const MovieCard = ({ movie: {Title, Year, Poster} }) => {
         <div style={{flex: "1"}}>
             <MovieTitle>{Title}</MovieTitle>
             <MovieRelease>{Year}</MovieRelease>
+            <ReadMore href='/'>Read More</ReadMore>
         </div>
        
     </Container>
