8bb3a9a054a54ffb8417ebee06ea34e401acf781
Brian Garton <gartonb@gmail.com>
2023-02-13 09:42:07 +0100

    react-parallax learnt

--- parallax/src/App.jsx
+++ parallax/src/App.jsx
@@ -1,32 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Parallax } from "react-parallax";
+import Rice from "/img/rice.jpg"
+import Monk from "/img/monk.jpg"
+import Beach from "/img/beach.jpg"
+import "./App.css"
+
+// https://github.com/rrutsche/react-parallax
+// Quick Docs
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="content">
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <Parallax strength={600} bgImage={Rice}>
+        <div className="parallax-content">
+          
+        </div>
+      </Parallax>
+      <div className="content">
+
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* Dynamic Blurring */}
+      <Parallax blur={{ min: -15, max: 15 }} strength={600} bgImage={Beach}>
+        <div className="parallax-content">
+          
+        </div>
+      </Parallax>
+      <div className="content">
+
+      </div>
+      {/* Reverse */}
+      <Parallax strength={-600} bgImage={Monk}>
+        <div className="parallax-content">
+          
+        </div>
+      </Parallax>
+      <div className="content">
+
+      </div>
     </div>
   )
 }
