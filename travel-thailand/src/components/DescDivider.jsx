bb9f32c45f0487cff71f684fd33f80a6a9ac9ef0
Brian Garton <gartonb@gmail.com>
2023-02-13 11:07:05 +0100

    DescDivder.jsx basic styling

---
+++ travel-thailand/src/components/DescDivider.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+    min-height: 60vh;
+    background-color: #ddd;
+    display: flex;
+
+`
+
+const ImgContainer = styled.div`
+    flex: 1;
+    background-image: ${props => props.image && `url(${props.image})`};
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+`
+
+const DescContainer = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 10px 4rem;
+`
+
+const Para = styled.p`
+    margin: 10px 0;
+`
+const DescDivider = ({ image, para1, para2, para3}) => {
+  return (
+    <Container>
+        <ImgContainer image={image}>
+            
+        </ImgContainer>
+        <DescContainer>
+            <Para>{para1}</Para>
+            <Para>{para2 && para2}</Para>
+            <Para>{para3 && para3}</Para>
+        </DescContainer>
+    </Container>
+  )
+}
+
+export default DescDivider
--- travel-thailand/src/pages/Home.jsx
+++ travel-thailand/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import { Parallax } from "react-parallax"
 import Title from '../components/Title'
 import LargeDivider from '../components/LargeDivider'
 import TextDivider from '../components/TextDivider'
+import DescDivider from '../components/DescDivider'
 
 const Container = styled.div`
 
@@ -20,6 +21,13 @@ const Home = () => {
         title="Hello World" 
         desc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, quo qui similique libero fugit esse aspernatur iusto. Tempore omnis, et quasi doloribus ea enim tempora quia officia eveniet porro natus incidunt ab mollitia, aspernatur molestias! Odit eaque amet beatae aut."
         />
+        <DescDivider image="/img/beach.jpg" title="Hello World" para2="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, quo qui similique libero fugit esse aspernatur iusto. Tempore omnis, et quasi doloribus ea enim tempora quia officia eveniet porro natus incidunt ab mollitia, aspernatur molestias! Odit eaque amet beatae aut." para3="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, quo qui similique libero fugit esse aspernatur iusto. Tempore omnis, et quasi doloribus ea enim tempora quia officia eveniet porro natus incidunt ab mollitia, aspernatur molestias! Odit eaque amet beatae aut."/>
+        <TextDivider 
+        title="Hello World" 
+        desc="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam, quo qui similique libero fugit esse aspernatur iusto. Tempore omnis, et quasi doloribus ea enim tempora quia officia eveniet porro natus incidunt ab mollitia, aspernatur molestias! Odit eaque amet beatae aut."
+        />
+        <LargeDivider image="/img/monk.jpg"/>
+        <DescDivider  title="Hello World" para1="lorem ifaisdjf aljsldf jlasd jflasidj lfiajsd ifjs idjfsijd fisjd "/>
     </Container>
   )
 }
