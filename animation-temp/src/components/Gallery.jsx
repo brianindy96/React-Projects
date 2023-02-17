9e88efad898f9fe7163a273a0eec5ea6318d185b
Brian Garton <gartonb@gmail.com>
2023-02-17 15:54:27 +0100

    Gallery and GalleryItem.jsx styled a bit

--- animation-temp/src/components/Gallery.jsx
+++ animation-temp/src/components/Gallery.jsx
@@ -1,8 +1,118 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import GalleryItem from './GalleryItem'
+
+const Section = styled.section`
+    background-color: #d53f41;
+    margin-left: -5vw;
+    position: relative;
+    margin-right: -5vw;
+`
+
+const GalleryCon = styled.div`
+  height: 70vh;
+  padding: 10vh 0;
+  width: 10000%;
+  display: flex;
+  flex-wrap: nowrap;
+`
+
+const GalleryCounter = styled.div`
+  position: absolute;
+  top: 10%;
+  left: 100px;
+  z-index: 1;
+  mix-blend-mode: difference;
+  line-height: 16px;
+  color: #dbd8d6;
+
+  font-family: "Bai Jamjuree";
+  font-weight: 600;
+
+  font-size: 16px;
+  -webkit-font-smoothing: antialiased;
+  display: inline-block;
+`
+
+const ActiveImg = styled.span`
+  
+`
+
+const Divider = styled.span`
+  content: '';
+  background-color: white;
+  width: 6.25vw;
+  margin: 7px 10px;
+  height: 1px;
+  display: inline-block;
+`
+
+const ImgCount = styled.span`
+  
+`
+
+// Images
+
+const images = [
+  {
+    id: 1,
+    src: "https://images.unsplash.com/photo-1566204773863-cf63e6d4ab88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1345&q=100",
+    title: "House Plant 1",
+    subtitle: "Living Room",
+    category: "Shooting/ Adv.Camping",
+  },
+  {
+    id: 2,
+    src: "https://images.unsplash.com/photo-1558603668-6570496b66f8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1300&q=100",
+    title: "House Plant 2",
+    subtitle: "Living Room",
+    category: "Shooting/ Adv.Camping",
+  },
+  {
+    id: 3,
+    src: "https://images.unsplash.com/photo-1567225557594-88d73e55f2cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=100",
+    title: "House Plant 3",
+    subtitle: "Living Room",
+    category: "Shooting/ Adv.Camping",
+  },
+  {
+    id: 4,
+    src: "/img/house4.jpg",
+    title: "House Plant 4",
+    subtitle: "Living Room",
+    category: "Shooting/ Adv.Camping",
+  },
+]
 
 const Gallery = () => {
+
+  const [activeImage, setActiveImage] = useState(1);
+
+  const handleUpdateActiveImage = (index) => {
+    setActiveImage(index + 1);
+  };
+
+
   return (
-    <div>Gallery</div>
+    <Section className='section-wrapper gallery-wrap'>
+      <GalleryCon>
+        <GalleryCounter>
+          <ActiveImg>{activeImage}</ActiveImg>
+          <Divider className="divider" />
+          <ImgCount>{images.length}</ImgCount>
+        </GalleryCounter>
+        {images.map((image) => (
+          <GalleryItem 
+          key={image.id} 
+          title={image.title} 
+          img={image.src} 
+          { ...image}
+          updateActiveImage={handleUpdateActiveImage}
+          />
+        ))}
+        <GalleryItem /> 
+      </GalleryCon>
+    </Section>
   )
 }
 
---
+++ animation-temp/src/components/GalleryItem.jsx
@@ -0,0 +1,101 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ItemWrapper = styled.div`
+    aspect-ratio: 16/9;
+    height: 100%;
+    display: grid;
+    grid-template-columns: 20vw 1fr 200px;
+    width: 100vw;
+
+    &.is-reveal {
+      .gallery-item-image {
+        transform: scale(1);
+        filter: none;
+      }
+    }
+`
+
+const ItemCon = styled.div`
+    width: 100%;
+    height: 100%;
+    position: relative;
+    will-change: transform;
+    
+`
+
+const ItemInfo = styled.div`
+    position: absolute;
+    bottom: 10%;
+    z-index: 1;
+    transform: translateX(-20%);
+    color: #dbd8d6;
+`
+
+const GalleryTitle = styled.h1`
+    line-height: 6vw;
+    font-family: "Bai Jamjuree";
+    font-weight: 600;
+
+    font-size: 6vw;
+    -webkit-font-smoothing: antialiased;
+`
+
+const GallerySubTitle = styled.h2`
+     position: relative;
+      line-height: 6vw;
+      color: transparent;
+      font-family: "Bodoni Moda";
+      font-weight: 400;
+      font-size: 6vw;
+      -webkit-font-smoothing: antialiased;
+      -webkit-text-stroke: 2px #dbd8d6;
+`
+
+const GalleryCategory = styled.p`
+      position: relative;
+      line-height: 24px;
+      font-family: "Bai Jamjuree";
+      font-weight: 400;
+      font-size: 24px;
+      -webkit-font-smoothing: antialiased;
+      margin-top: 2vh;
+`
+const GalleryImageCon = styled.div`
+    background-image: ${props => props.src && `url(${props.src})`};
+    background-size: cover;
+    background-position: center;
+    transform-origin: center;
+    width: 100%;
+    height: 100%;
+    will-change: transform;
+    transform: scale(0.7);
+    transition: all 1.5s cubic-bezier(0.77, 0, 0.175, 1);
+    filter: grayscale(100%) sepia(20%) brightness(80%);
+`
+
+const GalleryItem = ({
+    src,
+    category,
+    subtitle,
+    title,
+    updateActiveImage,
+    index,
+  }) => {
+  return (
+    <ItemWrapper className='gallery-item-wrapper'>
+        <div></div>
+        <ItemCon className={"gallery-item"}>
+            <ItemInfo className="gallery-item-info">
+            <GalleryTitle className="gallery-info-title">{title}</GalleryTitle>
+            <GallerySubTitle className="gallery-info-subtitle">{subtitle}</GallerySubTitle>
+            <GalleryCategory className="gallery-info-category">{category}</GalleryCategory>
+            </ItemInfo>
+            <GalleryImageCon src={src} className="gallery-item-image" />
+        </ItemCon>
+        <div></div>
+    </ItemWrapper>
+  )
+}
+
+export default GalleryItem
