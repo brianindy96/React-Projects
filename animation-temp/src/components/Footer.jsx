e3ea9d22a544e4d83bcc90418fba692df8a136e6
Brian Garton <gartonb@gmail.com>
2023-02-17 15:59:10 +0100

    Footer.jsx styled

--- animation-temp/src/components/Footer.jsx
+++ animation-temp/src/components/Footer.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import styled from 'styled-components'
+import SectionHeader from './SectionHeader'
 
+const Section = styled.div`
+  text-align: center;
+  padding-bottom: 100px;
+`
+const Location = styled.h1`
+  font-size: 18vw;
+  text-transform: uppercase;
+  font-family: 'Bodoni Moda', sans-serif;
+  margin: 0;
+`
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Section className='footer'>
+      
+      <SectionHeader title="Made in" />
+      <Location className='location' id='location-text'>
+        Chiang Mai
+      </Location>
+    </Section>
   )
 }
 
