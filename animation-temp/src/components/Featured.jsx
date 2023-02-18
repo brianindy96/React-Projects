dc5ce2ac472509c6b93e857fdadfbd86618f6c76
Brian Garton <gartonb@gmail.com>
2023-02-18 17:28:36 +0100

    Featured images changed to default pics

--- animation-temp/src/components/Featured.jsx
+++ animation-temp/src/components/Featured.jsx
@@ -60,11 +60,11 @@ const Featured = () => {
     >
       <FeaturedRow className="featured-row-layout">
         <Green>green</Green>
-        <GreenImage src="/img/green.jpg" />
+        <GreenImage src="https://images.unsplash.com/photo-1598838073192-05c942ede858?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=934&q=100" />
       </FeaturedRow>
       <FeaturedColumn className='featured-column-layout'>
         <Lily>lily</Lily>
-        <LilyImage src="/img/lily.jpg" />
+        <LilyImage src="https://images.unsplash.com/photo-1552248524-10d9a7e4841c?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=881&q=100" />
       </FeaturedColumn>
     </Section>
   )
