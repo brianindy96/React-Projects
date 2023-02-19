f01f827785a653e17960f5d2f021daa5f00eee3d
Brian Garton <gartonb@gmail.com>
2023-02-19 18:21:31 +0100

    World.jsx done and styled

--- metaverse-framer/sections/World.jsx
+++ metaverse-framer/sections/World.jsx
@@ -1,8 +1,36 @@
 'use client';
-
+import styles from "../styles/index"
+import { motion } from "framer-motion"
+import { fadeIn, staggerContainer } from "../utils/motion"
+import { TypingText, TitleText } from "../components/CustomTexts";
 const World = () => (
   <section>
-    World section
+    <motion.div
+    variants={staggerContainer}
+    initial="hidden"
+    whileInView="show"
+    viewport={{once: false, amount: 0.25}}
+    className={`${styles.innerWidth} mx-auto flex flex-col`}
+    >
+      <TypingText title="People of the World" textStyles="text-center"/>
+      <TitleText title={<>Track friends around you and invite them to play together in the same world</>} textStyles="text-center" />
+      <motion.div
+      variants={fadeIn('up', 'tween', 0.3, 1)}
+      className="relative mt-[68px] flex w-full h-[550px]"
+      >
+        <img src="/map.png" alt="map" className="w-full h-full object-cover" />
+        <div className="absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-01.png" alt="people" />
+        </div>  
+        <div className="absolute top-25 left-40 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-02.png" alt="people" />
+        </div>  
+        <div className="absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]">
+          <img src="/people-03.png" alt="people" />
+        </div>  
+        
+      </motion.div>
+    </motion.div>
   </section>
 );
 
