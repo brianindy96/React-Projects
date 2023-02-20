93742572279f7e4520f7345dbb8598ed27e1f287
Brian Garton <gartonb@gmail.com>
2023-02-20 23:10:51 +0100

    googleMapReacts props passed in but not complete

--- temp-google-maps/src/components/Map.jsx
+++ temp-google-maps/src/components/Map.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import styled from 'styled-components'
+import googleMapReact from 'google-map-react'
+import { Paper, Typography } from "@mui/material"
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import Rating from '@mui/material';
 const Map = () => {
+
+    const coord = { lat: 0, lng: 0 }
   return (
-    <div>Map</div>
+    <div>
+        <googleMapReact
+        bootstrapURLKeys={{ key: '' }}
+        defaultCenter={''}
+        center={coord}
+        defaultZoom={14}
+        // Top Left Bottom Right
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+        >
+
+        </googleMapReact>
+    </div>
   )
 }
 
