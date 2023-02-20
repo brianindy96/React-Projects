6f73975cf5a4b84d5524d2ead4d605b16d02ef8e
Brian Garton <gartonb@gmail.com>
2023-02-20 12:49:19 +0100

    nodemon as start module

--- temp-crud-library/backend/index.js
+++ temp-crud-library/backend/index.js
@@ -1,7 +1,7 @@
 import express from "express";
-
+// Nodemon helps with restarting application, everytime there is a change
 const app = express();
 
 app.listen(8800, () => {
-    console.log("Connected to Backend")
+    console.log("Connected to Backend!")
 });
