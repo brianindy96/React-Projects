ebb15ceef2efd08a002b499ac35c7bc66c3d8ffb
Brian Garton <gartonb@gmail.com>
2023-02-20 13:07:25 +0100

    .env file added and installed

--- temp-crud-library/backend/index.js
+++ temp-crud-library/backend/index.js
@@ -1,7 +1,21 @@
 import express from "express";
 // Nodemon helps with restarting application, everytime there is a change
+import mysql from "mysql"
+
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+const password = process.env.PASSWORD
+
+
 const app = express();
 
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: password,
+    database: "test",
+})
 app.listen(8800, () => {
     console.log("Connected to Backend!")
 });
