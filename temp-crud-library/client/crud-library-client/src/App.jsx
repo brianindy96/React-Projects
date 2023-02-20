a55602f2b76f4a51ab7a71ea7dac8d6068416d4a
Brian Garton <gartonb@gmail.com>
2023-02-20 15:37:22 +0100

    React-router-dom installed and react created

---
+++ temp-crud-library/client/crud-library-client/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
+function App() {
+
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route />
+          <Route />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
