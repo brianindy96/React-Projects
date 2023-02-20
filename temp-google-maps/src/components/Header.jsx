b92b820a440e070e2c8b13b51a3d4488704c955c
Brian Garton <gartonb@gmail.com>
2023-02-20 23:06:02 +0100

    fixed styling changed to styled-components

--- temp-google-maps/src/components/Header.jsx
+++ temp-google-maps/src/components/Header.jsx
@@ -1,13 +1,65 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from "styled-components"
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Typography, InputBase, Box } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search';
 
-const Container = styled.div`
+const Nav = styled.div`
+    width: 100vw;
+    height: 10vh;
+    background-color: #e3e3e3;
+    display: flex;
+    padding: 20px;
+    justify-content: space-between;
+    align-items: center;
+`
+const Toolbar = styled.div`
     
 `
 
+const SearchCon = styled.div`
+    display: flex;
+    margin: 0 20px;
+    border: 1px solid black;
+`
+
+const SearchIconCon = styled.div`
+    
+`
+
+
 const Header = () => {
+
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // setTimeout(() => {
+        //     setLoading(false);
+        // }, 3000)
+    }, [])
+    
   return (
-    <div>Header</div>
+    <Nav position="static">
+        <Toolbar>
+            <Typography variant="h5">
+                Travel Advisor
+            </Typography>
+        </Toolbar>
+        <Box display="flex">
+            <Typography variant="h6">
+                Explore
+            </Typography>
+             
+                {/* <Autocomplete> */}
+                <SearchCon>
+                    <SearchIconCon>
+                        <SearchIcon />
+                    </SearchIconCon>
+                    <InputBase placeholder="Search...." />
+                </SearchCon>
+                {/* </Autocomplete> */}
+        </Box>
+    </Nav>
   )
 }
 
