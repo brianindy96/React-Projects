6efe2d11fbf94618e2cf5f85053cab75cf7382f4
Brian Garton <gartonb@gmail.com>
2023-02-20 15:54:01 +0100

    Add New Book button added with Link to /add

--- temp-crud-library/client/crud-library-client/src/pages/Books.jsx
+++ temp-crud-library/client/crud-library-client/src/pages/Books.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios"
+import { Link } from 'react-router-dom';
 const Books = () => {
 
   const [books, setBooks] = useState([]);
@@ -38,6 +39,7 @@ const Books = () => {
             </div>
           ))}
         </div>
+        <button><Link to="/add">Add New Book</Link></button>
     </>
   )
 }
