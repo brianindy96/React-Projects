e9a912ee4af137efc4e800a94472a9bf4d5044a4
Brian Garton <gartonb@gmail.com>
2023-02-20 21:58:11 +0100

    App.jsx components layedout in Grids

--- temp-google-travel/src/App.jsx
+++ temp-google-travel/src/App.jsx
@@ -1,11 +1,24 @@
-import './App.css'
+import Header from './components/Header'
+import List from './components/List'
+import Map from './components/Map'
+import { CssBaseline, Grid } from "@mui/material";
 
 function App() {
 
   return (
-    <div className="App">
-        <h1>Hello World</h1>
-    </div>
+    <>
+       <CssBaseline />
+        <Header />
+        <Grid container spacing={3} style={{width: '100%'}}>
+          <Grid xs={12} md={4}>
+            <List />
+          </Grid>
+          <Grid xs={12} md={8}>
+            <Map />
+          </Grid>
+        </Grid>
+        
+    </>
   )
 }
 
