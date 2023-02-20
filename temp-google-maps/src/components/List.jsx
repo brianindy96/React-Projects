5a704b74a56b16c1bf9e51dd9a79f919bedfc1ed
Brian Garton <gartonb@gmail.com>
2023-02-20 23:57:19 +0100

    Search and Filters container

--- temp-google-maps/src/components/List.jsx
+++ temp-google-maps/src/components/List.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@mui/material'
+import styled from 'styled-components'
 
+const Container = styled.div`
+    flex: 1;
+    padding: 20px;
+
+`
+
+const FormControls = styled(FormControl)`
+    width: 500px;
+    margin-bottom: 30px;
+    
+`
 const List = () => {
+
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('');
   return (
-    <div style={{flex: 1}}>
-        List
-    </div>
+    <Container >
+        <Typography variant='h4'>Restaurants, Hotels & Attractions around you</Typography>
+        <FormControls >
+            <InputLabel>Type</InputLabel>
+            <Select value={type} onChange={(e) => setType(e.target.value)}>
+                <MenuItem value="restaurants">Restaurants</MenuItem>
+                <MenuItem value="hotels">Hotels</MenuItem>
+                <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+        </FormControls>
+        <FormControls >
+            <InputLabel>Rating</InputLabel>
+            <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+                <MenuItem value={0}>All</MenuItem>
+                <MenuItem value={3}>3 Stars</MenuItem>
+                <MenuItem value={4}>4 Stars</MenuItem>
+                <MenuItem value={5}>5 Stars</MenuItem>
+
+            </Select>
+        </FormControls>
+    </Container>
   )
 }
 
