8c59c39394ac0c8e6f90afd8b078d35ae4b36229
Brian Garton <gartonb@gmail.com>
2023-02-20 16:11:20 +0100

    Add.jsx books were posted to backend

--- temp-crud-library/client/crud-library-client/src/pages/Add.jsx
+++ temp-crud-library/client/crud-library-client/src/pages/Add.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 const Add = () => {
+    const [book, setBook] = useState({
+        title: "",
+        description: "",
+        cover: ""
+    });
+
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setBook((prev) => ({...prev, [e.target.name]: e.target.value}));
+    };
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+
+        try{
+            await axios.post("http://localhost:8800/books", book)
+            navigate("/");
+        }catch(err){
+            console.log(err);
+        }
+    }
+
+    console.log(book);
   return (
-    <div>Add</div>
+    <div className='form'>
+        <h1>Add New Book</h1>
+        <input type="text" placeholder='title' name="title" onChange={handleChange} />
+        <input type="text" placeholder='description' name="description" onChange={handleChange} />
+        <input type="text" placeholder='cover' name="cover" onChange={handleChange} />
+        <button onClick={handleSubmit}>Add</button>
+    </div>
   )
 }
 
