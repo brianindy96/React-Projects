8d7acf17ee2b633d81e7304a404ca0564a9ede58
Brian Garton <gartonb@gmail.com>
2023-02-20 15:03:45 +0100

    auth problems fixed

--- temp-crud-library/backend/index.js
+++ temp-crud-library/backend/index.js
@@ -17,9 +17,23 @@ const db = mysql.createConnection({
     database: "test",
 })
 
+
+
 // Use Express for API request with backend servers 
 app.get("/", (req, res)=> {
     res.json("hello this is the backend")
+})
+
+app.get("/books", (req, res)=> {
+    const q = "SELECT * FROM books"
+    
+    db.query(q,(err,data) => {
+        if(err){
+            return res.json(err);
+        } 
+        return res.json(data);
+        
+    });
 })
 
 
