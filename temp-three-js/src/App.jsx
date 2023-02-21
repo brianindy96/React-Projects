c9a42f7198d09ec1415085283ef0090a9d330fe7
Brian Garton <gartonb@gmail.com>
2023-02-22 00:27:40 +0100

    Stars from Drei and root height set to full

--- temp-three-js/src/App.jsx
+++ temp-three-js/src/App.jsx
@@ -1,14 +1,19 @@
 import './App.css'
 import { Canvas } from "@react-three/fiber"
 import Box from './components/Box'
-import { OrbitControls } from "@react-three/drei"
+import { OrbitControls, Stars } from "@react-three/drei"
 
 function App() {
 
   return (
-    <Canvas>
+    <Canvas style={{height: "100vh"}}>
       <OrbitControls />
+      <Stars />
       <ambientLight intensity={0.5} />
+      <spotLight 
+      position={[10,15,10]}
+      angle={0.3}
+      />
       <Box />
     </Canvas>
   )
