2c434a1393f1ed0af4b7ff6ae62ecffa9097c7ef
Brian Garton <gartonb@gmail.com>
2023-02-22 00:42:17 +0100

    ClickEvent for Box x,y,z

--- temp-three-js/src/components/Box.jsx
+++ temp-three-js/src/components/Box.jsx
@@ -2,10 +2,10 @@ import { useBox } from '@react-three/cannon'
 import React from 'react'
 
 const Box = () => {
-    const [ref] = useBox(() => ({ mass: 1}));
+    const [ref, api] = useBox(() => ({ mass: 1}));
 
   return (
-    <mesh ref={ref} position={[0,2,0]}>
+    <mesh onClick={() => api.velocity.set(1,2,0)} ref={ref} position={[0,2,0]}>
         <boxBufferGeometry attach="geometry"/>
         <meshLambertMaterial attach="material" color="hotpink" />
     </mesh>
