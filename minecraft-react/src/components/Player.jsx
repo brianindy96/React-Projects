2715b037d9734ec6f84874e4aaba7f5c16f42d5e
Brian Garton <gartonb@gmail.com>
2023-02-22 14:30:40 +0100

    update the useSphrere properties with useEffect, subscribing to the pos.current everytime the api.position changes

--- minecraft-react/src/components/Player.jsx
+++ minecraft-react/src/components/Player.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
 import { useSphere } from '@react-three/cannon';
 import { Vector3 } from 'three';
@@ -6,20 +6,31 @@ import { Vector3 } from 'three';
 
 const Player = () => {
 
-    const { camera } = useThree();
     
-    const [ref] = useSphere(() => ({
+    // The 'ref' object is used to attach the sphere to the Three.js Canvas
+    // the 'api' object is used to update the sphere's properties
+    const [ref, api] = useSphere(() => ({
         mass: 1,
         type: 'Dynamic',
+        // this position is set to change according to the pos.current
         position: [0,0,0]
     }));
 
     // Storing position of Player
+    // The useRef hook is called with an initial value of [0, 0, 0]. This creates a reference to a mutable value that can be used to store and access state across re-renders.
     const pos = useRef([0,0,0])
 
+    // An useEffect hook is used to subscribe to changes in the position of the sphere. When the position changes, the value of pos.current is updated to match the new position.
+    useEffect(() => {
+        api.position.subscribe((p) => pos.current = p)
+    }, [api.position])
     // This hook will run on everywhere
-    // Frame
+
+    // CAMERA
+    const { camera } = useThree();
+
     useFrame(() => {
+        // Every frame, the camera from Three copies the position used as Reference in an x,y,z  
         camera.position.copy(new Vector3(pos.current[0], pos.current[1], pos.current[2]))
     })
   return (
