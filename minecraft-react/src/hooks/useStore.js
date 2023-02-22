577532d9d03aaabc20edb5654de05ad648acae0f
Brian Garton <gartonb@gmail.com>
2023-02-22 22:30:00 +0100

    removeCube actions implemented

--- minecraft-react/src/hooks/useStore.js
+++ minecraft-react/src/hooks/useStore.js
@@ -29,7 +29,14 @@ export const useStore = create((set) => ({
             ]
         }))
     },
-    removeCube: () => {},
+    removeCube: (x,y,z) => {
+        set((prev) => ({
+            cubes: prev.cubes.filter(cube => {
+                const [X,Y,Z] = cube.pos
+                return X != x || Y != y || Z != z
+            })
+        }))
+    },
     setTexture: () => {},
     saveWorld: () => {},
 
