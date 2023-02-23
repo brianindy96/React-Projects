0e0368c616b6aa84895fd8377d37a6da4b6abcc6
Brian Garton <gartonb@gmail.com>
2023-02-23 10:37:38 +0100

    CRUD service functions added in book.service.js

--- temp-crud-firebase-library/src/firebase-config.js
+++ temp-crud-firebase-library/src/firebase-config.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
@@ -14,3 +15,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
---
+++ temp-crud-firebase-library/src/services/book.service.js
@@ -0,0 +1,35 @@
+import { db } from "../firebase-config"
+// Methods from firestore to access and work on data in store
+import { collection, 
+         getDocs, 
+         getDoc, 
+         addDoc, 
+         updateDoc, 
+         deleteDoc, 
+         doc 
+        } from "firebase/firestore"
+
+const bookCollectionRef = collection(db, "books");
+
+export const addBooks = (newBook) => {
+    return addDoc(bookCollectionRef, newBook);
+}
+
+export const updateBooks = (id, updatedBook) => {
+    const bookDoc = doc(db, "books", id);
+    return updateDoc(bookDoc, updatedBook);
+}
+
+export const deleteBook = (id) => {
+    const bookDoc = doc(db, "books", id);
+    return deleteDoc(bookDoc)
+}
+
+export const getAllBooks = () => {
+    return getDocs(bookCollectionRef)
+}
+
+export const getBook = (id) => {
+    // doc(firestore_reference, collection_name, id)
+    const bookDoc = doc(db, "books", id)
+}
