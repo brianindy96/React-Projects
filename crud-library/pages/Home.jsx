9fe0b31d3c68e198c24df209e127f30b38b7026b
Brian Garton <gartonb@gmail.com>
2023-02-23 20:52:29 +0100

    applied Router to App.jsx for navigation between other pages

---
+++ crud-library/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='container'>
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        
+    </div>
+  )
+}
+
+export default Home
