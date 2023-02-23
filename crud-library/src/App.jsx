aa1ad504b5ba6377b002feccba1b5ce1f89f5a7b
Brian Garton <gartonb@gmail.com>
2023-02-23 21:06:34 +0100

    installed Tailwind

--- crud-library/src/App.jsx
+++ crud-library/src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css'
-import { Routes, Route } from "react-router-dom"
-import Home from '../pages/Home'
-import Add from '../pages/Add'
-import Edit from '../pages/Edit'
-
 
 function App() {
-  
 
   return (
     <div className="App">
-        <Routes>
-          <Route path="/" exact element={<Home />} />
-          <Route path="/add"  element={<Add />} />
-          <Route path="/edit"  element={<Edit />} />
-        </Routes>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   )
 }
