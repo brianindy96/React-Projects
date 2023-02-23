d34c3c4abc8da56ba7fc4a4157281430a6ca2ef1
Brian Garton <gartonb@gmail.com>
2023-02-23 21:09:42 +0100

    applied Router for navigation over pages

--- crud-library/src/App.jsx
+++ crud-library/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.css'
+import { Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import Add from './pages/Add'
+import Edit from './pages/Edit'
 
 function App() {
 
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/add' element={<Add />} />
+        <Route path='/edit' element={<Edit />} />
+      </Routes>
     </div>
   )
 }
---
+++ crud-library/src/pages/Edit.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Edit = () => {
+  return (
+    <div>
+        <h1 className="text-3xl font-bold underline">
+            Edit
+        </h1>
+    </div>
+  )
+}
+
+export default Edit
