ea5ffe9b8c5978e262ada84018c3c71bd72dc708
Brian Garton <gartonb@gmail.com>
2023-02-24 14:03:15 +0100

    Controlled Radio Groups, now onSubmit read: boolean is added to submitted data

--- crud-library/src/pages/Add.jsx
+++ crud-library/src/pages/Add.jsx
@@ -1,34 +1,49 @@
 import React from 'react'
 import { Typography, Paper, Button, CircularProgress, Radio } from '@mui/material';
 import FormInput from '../components/FormInput';
-import { useForm, FormProvider } from "react-hook-form"
+import { useForm, FormProvider, Controller, useFormContext } from "react-hook-form"
 import { FormLabel, RadioGroup, FormControlLabel, FormControl } from "@mui/material"
 import { Link } from 'react-router-dom';
+
 const Add = () => {
     const methods = useForm();
+
+    const { handleSubmit } =  methods;
+    
+    const onSubmit = (data) =>{
+        console.log(data)
+    }
+
+    const { control } = useFormContext;
 
   return (
     <div className='max-w-7xl m-auto h-95' id='main-container'>
         <div className='h-95 flex justify-center items-center' id='wrapper'>
         <Paper sx={{ display: "flex", flexDirection: "column", height: "60vh", padding: "20px", minWidth: "720px", margin: "20px 0px", textAlign: "center", alignItems: "center", justifyContent: "center"}}>
                 <FormProvider { ...methods }>
-                    <form style={{display: "flex", flexDirection: "column",  justifyContent: "center", alignItems: "center"}}>
+                    <form onSubmit={handleSubmit(onSubmit)} style={{display: "flex", flexDirection: "column",  justifyContent: "center", alignItems: "center"}}>
                         <h1 className='text-4xl font-bold mb-5'>Add a book</h1>
                         <FormInput required name="title" label="Title" type="text" />
                         <FormInput required name="author" label="Author" type="text" />
                         <FormInput required name="pages" label="Pages" type="number" />
                         <div style={{display: "flex", alignItems: "center"}}>
-                            <FormLabel className='my-2 mx-2 text-black' id="demo-radio-buttons-group-label">Read</FormLabel>
+                        <Controller
+                            control={control}
+                            name="read"
+                            render={({ field }) => (
                             <RadioGroup
-                                aria-labelledby="demo-radio-buttons-group-label"
-                                defaultValue="female"
-                                name="radio-buttons-group"
+                                aria-label="Read"
+                                {...field}
+                                value={field.value ? "true" : "false"}
+                                onChange={(event) => field.onChange(event.target.value === "true")}
                             >
                                 <div>
-                                    <FormControlLabel className='px-6' value={true} control={<Radio />} label="Yes" />
-                                    <FormControlLabel value={false} control={<Radio />} label="No"  />
+                                <FormControlLabel className='px-6' value="true" control={<Radio />} label="Yes" />
+                                <FormControlLabel value="false" control={<Radio />} label="No"  />
                                 </div>
                             </RadioGroup>
+                            )}
+                        />
                         </div>
                         <div className='flex flex-col mt-2 place-items-center ml-2'>
                             <button className='bg-blue-400 rounded-lg text-md hover:bg-blue-300 py-2 mt-2 px-28' type="submit">Add Book</button>
